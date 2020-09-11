f3903ddf09c478e65fc44b82267d646266fa293b
TheRealFlash <john.maliha@polymtl.ca>
2020-09-11 15:55:25 -0400

    Correction d'erreurs: lint

--- client/src/app/user-guide/user-guide.component.spec.ts
+++ client/src/app/user-guide/user-guide.component.spec.ts
@@ -1,25 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserGuideComponent } from './user-guide.component';
 
 describe('UserGuideComponent', () => {
-  let component: UserGuideComponent;
-  let fixture: ComponentFixture<UserGuideComponent>;
+    let component: UserGuideComponent;
+    let fixture: ComponentFixture<UserGuideComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ UserGuideComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [UserGuideComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(UserGuideComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(UserGuideComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
