fcb65b3ab0681d8a9821d4bda808fc42d93b6f65
alphonse <alphonse.desrosiers@polymtl.ca>
2020-09-11 17:05:03 -0400

    path user-guide de app-routing corriger

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditorComponent } from './components/editor/editor.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
-import { UserGuideComponent } from './user-guide/user-guide.component';
+import { UserGuideComponent } from './components/user-guide/user-guide.component';
 
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
