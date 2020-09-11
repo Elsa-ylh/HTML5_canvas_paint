314dcace66a53793c43d82d0e16b30fd2b7a4a2d
alphonse <alphonse.desrosiers@polymtl.ca>
2020-09-11 16:51:58 -0400

    creation interface et le data du guide utilisation

--- client/src/app/guide-information-picture.spec.ts
+++
@@ -1,7 +0,0 @@
-import { GuideInformationPicture } from './guide-information-picture';
-
-describe('Guideinformation', () => {
-    it('should create an instance', () => {
-        expect(new GuideInformationPicture('est_le_Titre', 'est un paragephe', 'est_le_nom_image')).toBeTruthy();
-    });
-});
--- client/src/app/guide-information-video.ts
+++
@@ -1,8 +0,0 @@
-import { AbsGuideInformation } from './classes/abs-guide-information';
-
-export class GuideInformationVideo extends AbsGuideInformation {
-    constructor(title: string, paragraphInformation: string, elementName: string) {
-        super(title, paragraphInformation, elementName);
-    }
-}
-// le tipe de video non déssidé en équipe donc vide pour lintent
