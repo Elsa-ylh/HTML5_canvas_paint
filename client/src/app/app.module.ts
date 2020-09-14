4acc236356a317ba079ecf4db192b6c52cb9af9c
Tai Chen Li <dev@taichen.ca>
2020-09-13 22:09:44 -0400

    add new drawing inside the app

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -49,13 +49,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { DialogCreateNewDrawingComponent } from './components/dialog-create-new-drawing/dialog-create-new-drawing.component';
+import { DialogNewDrawingComponent } from './components/dialog-new-drawing/dialog-new-drawing.component';
 import { DrawingComponent } from './components/drawing/drawing.component';
 import { EditorComponent } from './components/editor/editor.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-    declarations: [AppComponent, EditorComponent, SidebarComponent, DrawingComponent, MainPageComponent, DialogCreateNewDrawingComponent],
+    declarations: [
+        AppComponent,
+        EditorComponent,
+        SidebarComponent,
+        DrawingComponent,
+        MainPageComponent,
+        DialogCreateNewDrawingComponent,
+        DialogNewDrawingComponent,
+    ],
     imports: [
         FormsModule,
         BrowserModule,
--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { DialogCreateNewDrawingComponent } from '@app/components/dialog-create-new-drawing/dialog-create-new-drawing.component';
 import { DialogNewDrawingComponent } from '@app/components/dialog-new-drawing/dialog-new-drawing.component';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
@@ -9,16 +10,16 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
     styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
-    constructor(private drawingService: DrawingService, private dialog: MatDialog) {}
-
-    openDialog(): void {
-        this.dialog.open(DialogNewDrawingComponent);
-    }
+    constructor(private drawingService: DrawingService, private dialogClear: MatDialog, private dialogNewDrawing: MatDialog) {}
 
     clearCanvas(): void {
         if (!this.drawingService.isCanvasBlank()) {
-            this.openDialog();
+            this.dialogClear.open(DialogNewDrawingComponent);
         }
+    }
+
+    createNewDrawing(): void {
+        this.dialogNewDrawing.open(DialogCreateNewDrawingComponent);
     }
 
     // keybind control o for new drawing
