c95b5813c7b24a8cb6efa03c0960845ea1ab24bb
Tai Chen Li <dev@taichen.ca>
2020-09-15 19:24:49 -0400

    merge and fix of canvas

--- client/src/app/services/tools/pencil-service.ts
+++ client/src/app/services/tools/pencil-service.ts
@@ -38,18 +38,16 @@ export class PencilService extends Tool {
     }
 
     onMouseUp(event: MouseEvent): void {
-        this.clearPath();
-
         if (this.mouseDown) {
             const mousePosition = this.getPositionFromMouse(event);
             this.pathData.push(mousePosition);
             this.drawLine(this.drawingService.baseCtx, this.pathData);
         }
         this.mouseDown = false;
+        this.clearPath();
     }
 
     onMouseMove(event: MouseEvent): void {
-       
         if (this.mouseDown) {
             const mousePosition = this.getPositionFromMouse(event);
             this.pathData.push(mousePosition);
