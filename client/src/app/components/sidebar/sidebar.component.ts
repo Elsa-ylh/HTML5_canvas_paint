bb9b717749af4cf33b7e703f3333481d76cbca1e
alphonse <alphonse.desrosiers@polymtl.ca>
2020-09-17 10:11:10 -0400

    fix merge et create btn different brush et btn size

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { DialogCreateNewDrawingComponent } from '@app/components/dialog-create-new-drawing/dialog-create-new-drawing.component';
+import { DrawingService } from '@app/services/drawing/drawing.service';
 import { SwitchToolService } from '@app/services/switchTool-service';
 
 @Component({
@@ -6,9 +11,34 @@ import { SwitchToolService } from '@app/services/switchTool-service';
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
-    constructor(private switchToolServ: SwitchToolService) {}
-
+export class SidebarComponent implements OnInit {
+    constructor(
+        private switchToolServ: SwitchToolService,
+        private drawingService: DrawingService,
+        private dialogNewDrawing: MatDialog,
+        private iconRegistry: MatIconRegistry,
+        private sanitizer: DomSanitizer,
+    ) {}
+    ngOnInit(): void {
+        this.iconRegistry.addSvgIcon('eraser', this.sanitizer.bypassSecurityTrustResourceUrl('assets/clarity_eraser-solid.svg'));
+    }
     SetPencil = () => this.switchToolServ.switchTool(0);
     SetEraser = () => this.switchToolServ.switchTool(1);
+    clearCanvas(): void {
+        if (!this.drawingService.isCanvasBlank()) {
+            this.dialogNewDrawing.open(DialogCreateNewDrawingComponent);
+        }
+    }
+
+    createNewDrawing(): void {
+        this.dialogNewDrawing.open(DialogCreateNewDrawingComponent);
+    }
+
+    // keybind control o for new drawing
+    @HostListener('window:keydown', ['$event']) onKeyDown(o: KeyboardEvent): void {
+        o.preventDefault();
+        if (o.ctrlKey && o.code === 'KeyO') {
+            this.clearCanvas();
+        }
+    }
 }
