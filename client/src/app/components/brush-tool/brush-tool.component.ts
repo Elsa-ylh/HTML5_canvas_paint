861f72144cb504db328bf14bb4f80d820890c101
alphonse <alphonse.desrosiers@polymtl.ca>
2020-09-17 11:34:26 -0400

    new brush service et add et

--- client/src/app/components/brush-tool/brush-tool.component.ts
+++ client/src/app/components/brush-tool/brush-tool.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ServiceTool, SwitchToolService } from '@app/services/switchTool-service';
 
 @Component({
     selector: 'app-brush-tool',
@@ -6,8 +7,19 @@ import { Component } from '@angular/core';
     styleUrls: ['./brush-tool.component.scss'],
 })
 export class BrushToolComponent {
+    @ViewChild('baseCanvas', { static: false }) baseCanvas: ElementRef<HTMLCanvasElement>;
+    // On utilise ce canvas pour dessiner sans affecter le dessin final
+    @ViewChild('previewCanvas', { static: false }) previewCanvas: ElementRef<HTMLCanvasElement>;
+
+    /* private baseCtx: CanvasRenderingContext2D;
+    private previewCtx: CanvasRenderingContext2D;
+    private canvasSize: Vec2 = { x: 250, y: 250 }; // je ne suis pas sur que sa doit être la ca sa affete tous le dessin peut inporte le component
+*/
+    constructor(private switchToolServ: SwitchToolService) {
+        this.naturalBrushTool();
+    }
     // Parmet d'avoir un pinceau
     naturalBrushTool() {
-        // activer pinceau
+        this.switchToolServ.switchTool(ServiceTool.brushServie);
     }
 }
--- client/src/app/services/switchTool-service.ts
+++ client/src/app/services/switchTool-service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Tool } from '@app/classes/tool';
+import { BrushService } from '@app/services/tools/brush.service';
 import { EraserService } from '@app/services/tools/eraser-service';
 import { PencilService } from '@app/services/tools/pencil-service';
-
 @Injectable({
     providedIn: 'root',
 })
@@ -10,11 +10,16 @@ export class SwitchToolService {
     currentTool: Tool;
     TableTool: Tool[];
 
-    constructor(protected pencilService: PencilService, eraserService: EraserService) {
-        this.TableTool = [pencilService, eraserService]; // put all the services of tools here.
+    constructor(protected pencilService: PencilService, eraserService: EraserService, brushServie: BrushService) {
+        this.TableTool = [pencilService, eraserService, brushServie]; // put all the services of tools here.
     }
 
     public switchTool(num: number): void {
         this.currentTool = this.TableTool[num];
     }
+}
+export enum ServiceTool {
+    pencilService = 1,
+    eraserService = 2,
+    brushServie = 3,
 }
