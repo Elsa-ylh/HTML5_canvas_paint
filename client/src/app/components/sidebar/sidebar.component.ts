84496013c52a87dba628581b259a963f3c21fc78
Tai Chen Li <dev@taichen.ca>
2020-09-17 21:29:34 -0400

    alpha integration of pencil

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -5,6 +5,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { ToolUsed } from '@app/classes/tool';
 import { DialogCreateNewDrawingComponent } from '@app/components/dialog-create-new-drawing/dialog-create-new-drawing.component';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { PencilService } from '@app/services/tools/pencil-service';
 import { RectangleService } from '@app/services/tools/rectangle.service';
 
 @Component({
@@ -45,6 +46,7 @@ export class SidebarComponent implements OnInit {
     }
 
     pickPencil(): void {
+        this.drawingService.currentTool = new PencilService(this.drawingService);
         this.drawingService.whichTools = ToolUsed.Pencil;
     }
 
