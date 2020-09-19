d5f05ead8f4c381e99449c4cda98ca835e60def1
Tai Chen Li <dev@taichen.ca>
2020-09-18 21:47:50 -0400

    no more linting array error

--- client/src/app/services/tool-service.ts
+++ client/src/app/services/tool-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Tool, ToolUsed } from '@app/classes/tool';
 import { EraserService } from '@app/services/tools/eraser-service';
 import { PencilService } from '@app/services/tools/pencil-service';
+import { RectangleService } from '@app/services/tools/rectangle.service';
 
 @Injectable({
     providedIn: 'root',
@@ -9,17 +10,15 @@ import { PencilService } from '@app/services/tools/pencil-service';
 export class ToolService {
     currentToolName: ToolUsed;
     currentTool: Tool;
-    private tableTool: Array<Tool> = new Array<Tool>();
+    private tableTool: Tool[] = new Array();
 
-    constructor(protected pencilService: PencilService, protected eraserService: EraserService) {
+    constructor(protected pencilService: PencilService, protected eraserService: EraserService, protected rectangleService: RectangleService) {
         this.tableTool[ToolUsed.NONE] = pencilService;
         this.tableTool[ToolUsed.Pencil] = pencilService;
         this.tableTool[ToolUsed.Eraser] = eraserService;
-        // this.TableTool[ToolUsed.Brush] =
-        // this.TableTool[ToolUsed.Line] =
-        // this.TableTool[ToolUsed.Rectangle] =
-        // this.TableTool[ToolUsed.Ellipse] =
-        // this.TableTool[ToolUsed.Color] =
+        // this.tableTool[ToolUsed.Brush] = brushService;
+        // this.tableTool[ToolUsed.Line] = lineService;
+        this.tableTool[ToolUsed.Rectangle] = rectangleService;
     }
 
     switchTool(toolUsed: ToolUsed): void {
