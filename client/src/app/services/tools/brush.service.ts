0cb5e6fafd1be5089b5e2a8bc8694795e17287c2
alphonse <alphonse.desrosiers@polymtl.ca>
2020-09-23 22:05:27 -0400

    3 out of 5 brush functionality works

--- client/src/app/services/tools/brush.service.ts
+++ client/src/app/services/tools/brush.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { MouseButton } from '@app/classes/mouse-button';
+import { SubToolselected } from '@app/classes/sub-tool-selected';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+const motionDifference = 5; // le numbre va faire marcher le ThichBrush
 
 @Injectable({
     providedIn: 'root',
 })
 export class BrushService extends Tool {
     pixelThickness: number = 4;
-
+    private lastPoint: Vec2;
     private pathData: Vec2[];
 
     constructor(drawingService: DrawingService) {
@@ -18,21 +20,19 @@ export class BrushService extends Tool {
     }
 
     onMouseDown(event: MouseEvent): void {
+        this.witchBrush(this.subToolSelect);
         this.mouseDown = event.button === MouseButton.Left;
         if (this.mouseDown) {
-            this.drawingService.baseCtx.strokeStyle = '#000000'; // to draw after erasing
-            this.drawingService.previewCtx.strokeStyle = '#000000';
-            // this.drawingService.baseCtx.lineWidth = startpx; // conserve same size a before
-            // this.drawingService.previewCtx.lineWidth = startpx;
             this.clearPath();
             this.mouseDownCoord = this.getPositionFromMouse(event);
             this.pathData.push(this.mouseDownCoord);
-            // console.log('bruch');
+            this.lastPoint = this.getPositionFromMouse(event);
         }
     }
 
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
+            console.log('test onMouse and subtool ', this.subToolSelect);
             const mousePosition = this.getPositionFromMouse(event);
             this.pathData.push(mousePosition);
             this.drawLine(this.drawingService.baseCtx, this.pathData);
@@ -53,14 +53,83 @@ export class BrushService extends Tool {
     }
 
     private drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
-        ctx.beginPath();
-        for (const point of path) {
-            ctx.lineTo(point.x, point.y);
+        switch (this.subToolSelect) {
+            case SubToolselected.tool1:
+                ctx.beginPath();
+                for (const point of path) {
+                    ctx.lineTo(point.x, point.y);
+                }
+                ctx.stroke();
+                break;
+            case SubToolselected.tool2:
+                ctx.beginPath();
+                for (const point of path) {
+                    ctx.lineTo(point.x, point.y);
+                }
+                ctx.stroke();
+                break;
+            case SubToolselected.tool3:
+                ctx.beginPath();
+                this.lastPoint = path[0];
+                for (const point of path) {
+                    ctx.beginPath();
+                    ctx.moveTo(this.lastPoint.x, this.lastPoint.y);
+                    ctx.lineTo(point.x, point.y);
+                    ctx.stroke();
+
+                    ctx.moveTo(this.lastPoint.x - motionDifference, this.lastPoint.y - motionDifference);
+                    ctx.lineTo(point.x - motionDifference, point.y - motionDifference);
+                    ctx.stroke();
+
+                    this.lastPoint = point;
+                }
+                break;
+            case SubToolselected.tool4:
+                break;
+            case SubToolselected.tool5:
+                break;
+            default:
+                break;
         }
-        ctx.stroke();
+    }
+    private witchBrush(select: number): void {
+        this.clearEffectTool();
+        switch (select) {
+            case SubToolselected.tool1:
+                this.drawingService.baseCtx.lineJoin = this.drawingService.baseCtx.lineCap = 'round';
+                this.drawingService.previewCtx.lineJoin = this.drawingService.previewCtx.lineCap = 'round';
+                break;
+            case SubToolselected.tool2:
+                this.drawingService.baseCtx.lineJoin = this.drawingService.baseCtx.lineCap = 'round';
+                this.drawingService.previewCtx.lineJoin = this.drawingService.previewCtx.lineCap = 'round';
+                this.drawingService.baseCtx.shadowColor = 'rgb(0, 0, 0)';
+                this.drawingService.previewCtx.shadowColor = 'rgb(0, 0, 0)';
+                this.drawingService.baseCtx.shadowBlur = this.drawingService.baseCtx.lineWidth + 2;
+                this.drawingService.previewCtx.shadowBlur = this.drawingService.baseCtx.lineWidth + 2;
+                break;
+            case SubToolselected.tool3:
+                this.drawingService.baseCtx.lineJoin = this.drawingService.previewCtx.lineJoin = 'bevel';
+                this.drawingService.baseCtx.lineCap = this.drawingService.previewCtx.lineCap = 'butt';
+                break;
+            case SubToolselected.tool4:
+                break;
+            case SubToolselected.tool5:
+                break;
+            default:
+                break;
+        }
     }
 
     private clearPath(): void {
         this.pathData = [];
+    }
+    private clearEffectTool(): void {
+        this.drawingService.baseCtx.shadowColor = 'rgba(0,0,0,0)';
+        this.drawingService.previewCtx.shadowColor = 'rgba(0,0,0,0)';
+        /*this.drawingService.baseCtx.lineWidth = this.pxSize;
+        this.drawingService.previewCtx.lineWidth = this.pxSize;*/
+        this.drawingService.baseCtx.strokeStyle = '#000000'; // to draw after erasing
+        this.drawingService.previewCtx.strokeStyle = '#000000';
+        this.drawingService.baseCtx.lineJoin = this.drawingService.baseCtx.lineCap = 'round';
     }
 }
