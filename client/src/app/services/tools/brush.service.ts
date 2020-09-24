fdb57533a9e58da73312402f841b2d6a90e1cfc6
alphonse <alphonse.desrosiers@polymtl.ca>
2020-09-24 00:47:48 -0400

    4 out of 5 brush functionality works

--- client/src/app/services/tools/brush.service.ts
+++ client/src/app/services/tools/brush.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { MouseButton } from '@app/classes/mouse-button';
+import { PointArc } from '@app/classes/point-arc';
 import { SubToolselected } from '@app/classes/sub-tool-selected';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { DrawingService } from '@app/services/drawing/drawing.service';
-const motionDifference = 5; // le numbre va faire marcher le ThichBrush
-
+const motionDifference = 4; // le numbre va faire marcher le ThichBrush
+const citcle = Math.PI * 2;
 @Injectable({
     providedIn: 'root',
 })
@@ -13,7 +14,7 @@ export class BrushService extends Tool {
     pixelThickness: number = 4;
     private lastPoint: Vec2;
     private pathData: Vec2[];
-
+    private brush4Data: PointArc[];
     constructor(drawingService: DrawingService) {
         super(drawingService);
         this.clearPath();
@@ -25,17 +26,27 @@ export class BrushService extends Tool {
         if (this.mouseDown) {
             this.clearPath();
             this.mouseDownCoord = this.getPositionFromMouse(event);
-            this.pathData.push(this.mouseDownCoord);
-            this.lastPoint = this.getPositionFromMouse(event);
+            if (this.subToolSelect === SubToolselected.tool4) {
+                const point = new PointArc(this.mouseDownCoord, this.remdomInt(), Math.random());
+                this.brush4Data.push(point);
+            } else {
+                this.pathData.push(this.mouseDownCoord);
+                this.lastPoint = this.getPositionFromMouse(event);
+            }
         }
     }
 
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
-            console.log('test onMouse and subtool ', this.subToolSelect);
             const mousePosition = this.getPositionFromMouse(event);
-            this.pathData.push(mousePosition);
-            this.drawLine(this.drawingService.baseCtx, this.pathData);
+            if (this.subToolSelect === SubToolselected.tool4) {
+                const point = new PointArc(mousePosition, this.remdomInt(), Math.random());
+                this.brush4Data.push(point);
+                this.drawBrushTool4(this.drawingService.baseCtx, this.brush4Data);
+            } else {
+                this.pathData.push(mousePosition);
+                this.drawLine(this.drawingService.baseCtx, this.pathData);
+            }
         }
         this.mouseDown = false;
         this.clearPath();
@@ -44,12 +55,28 @@ export class BrushService extends Tool {
     onMouseMove(event: MouseEvent): void {
         if (this.mouseDown) {
             const mousePosition = this.getPositionFromMouse(event);
-            this.pathData.push(mousePosition);
 
             // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
-            this.drawLine(this.drawingService.previewCtx, this.pathData);
+            if (this.subToolSelect === SubToolselected.tool4) {
+                const point = new PointArc(mousePosition, this.remdomInt(), Math.random());
+                this.brush4Data.push(point);
+                this.drawBrushTool4(this.drawingService.previewCtx, this.brush4Data);
+            } else {
+                this.pathData.push(mousePosition);
+                this.drawLine(this.drawingService.previewCtx, this.pathData);
+            }
         }
+    }
+    private drawBrushTool4(ctx: CanvasRenderingContext2D, path: PointArc[]) {
+        ctx.beginPath();
+        for (const point of path) {
+            ctx.beginPath();
+            ctx.globalAlpha = point.opacity;
+            ctx.arc(point.vec2.x, point.vec2.y, point.radius, 0, citcle);
+            ctx.fill();
+        }
+        ctx.stroke();
     }
 
     private drawLine(ctx: CanvasRenderingContext2D, path: Vec2[]): void {
@@ -85,6 +112,8 @@ export class BrushService extends Tool {
                 }
                 break;
             case SubToolselected.tool4:
+                // if (ctx === this.drawingService.previewCtx) break;
+
                 break;
             case SubToolselected.tool5:
                 break;
@@ -104,14 +133,19 @@ export class BrushService extends Tool {
                 this.drawingService.previewCtx.lineJoin = this.drawingService.previewCtx.lineCap = 'round';
                 this.drawingService.baseCtx.shadowColor = 'rgb(0, 0, 0)';
                 this.drawingService.previewCtx.shadowColor = 'rgb(0, 0, 0)';
-                this.drawingService.baseCtx.shadowBlur = this.drawingService.baseCtx.lineWidth + 2;
-                this.drawingService.previewCtx.shadowBlur = this.drawingService.baseCtx.lineWidth + 2;
+                this.drawingService.baseCtx.shadowBlur = this.drawingService.baseCtx.lineWidth;
+                this.drawingService.previewCtx.shadowBlur = this.drawingService.baseCtx.lineWidth;
                 break;
             case SubToolselected.tool3:
                 this.drawingService.baseCtx.lineJoin = this.drawingService.previewCtx.lineJoin = 'bevel';
                 this.drawingService.baseCtx.lineCap = this.drawingService.previewCtx.lineCap = 'butt';
                 break;
             case SubToolselected.tool4:
+                this.drawingService.baseCtx.lineCap = 'butt';
+                this.drawingService.baseCtx.lineJoin = 'miter';
+                this.drawingService.previewCtx.lineCap = 'butt';
+                this.drawingService.previewCtx.lineJoin = 'miter';
+
                 break;
             case SubToolselected.tool5:
                 break;
@@ -122,6 +156,7 @@ export class BrushService extends Tool {
 
     private clearPath(): void {
         this.pathData = [];
+        this.brush4Data = [];
     }
     private clearEffectTool(): void {
         this.drawingService.baseCtx.shadowColor = 'rgba(0,0,0,0)';
@@ -131,5 +166,10 @@ export class BrushService extends Tool {
         this.drawingService.baseCtx.strokeStyle = '#000000'; // to draw after erasing
         this.drawingService.previewCtx.strokeStyle = '#000000';
         this.drawingService.baseCtx.lineJoin = this.drawingService.baseCtx.lineCap = 'round';
+    }
+    private remdomInt(): number {
+        const min = this.drawingService.baseCtx.lineWidth / 8;
+        const differnve = this.drawingService.baseCtx.lineWidth - min;
+        return Math.floor(Math.random() * differnve) + min;
     }
 }
