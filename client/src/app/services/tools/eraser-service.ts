60e020500eba6521901b77017f4718ba2c903595
TheRealFlash <john.maliha@polymtl.ca>
2020-09-25 20:50:25 -0400

    Merged dev into eraser. Everything working. Eraser Done.

--- client/src/app/services/tools/eraser-service.ts
+++ client/src/app/services/tools/eraser-service.ts
@@ -24,11 +24,8 @@ export class EraserService extends Tool {
             this.mouseMove = false;
             this.drawingService.baseCtx.strokeStyle = '#FFF'; // draw in white
             this.drawingService.previewCtx.strokeStyle = '#FFF'; // when changecolor is implemented call pencil weith white.
-            this.drawingService.baseCtx.lineWidth = this.minimalPx; // minimal size is 5 px.
-            this.drawingService.previewCtx.lineWidth = this.minimalPx;
             this.drawingService.baseCtx.setLineDash([0, 0]); // reset
             this.drawingService.previewCtx.setLineDash([0, 0]); // reset
-
             this.mouseDownCoord = this.getPositionFromMouse(event);
             this.pathData.push(this.mouseDownCoord);
         }
@@ -40,13 +37,15 @@ export class EraserService extends Tool {
             if (this.mouseMove) {
                 this.pathData.push(mousePosition);
                 this.RemoveLine(this.drawingService.baseCtx, this.pathData);
+                this.RemoveLine(this.drawingService.previewCtx, this.pathData);
             } else {
                 // code to draw dot
                 this.drawingService.baseCtx.fillStyle = '#FFF';
                 this.drawingService.baseCtx.fillRect(mousePosition.x, mousePosition.y, this.minimalPx, this.minimalPx);
+                this.drawingService.previewCtx.fillStyle = '#FFF';
+                this.drawingService.previewCtx.fillRect(mousePosition.x, mousePosition.y, this.minimalPx, this.minimalPx);
             }
         }
-
         this.mouseDown = false;
         this.clearPath();
     }
@@ -60,6 +59,7 @@ export class EraserService extends Tool {
             // On dessine sur le canvas de prévisualisation et on l'efface à chaque déplacement de la souris
             this.drawingService.clearCanvas(this.drawingService.previewCtx);
             this.RemoveLine(this.drawingService.previewCtx, this.pathData);
+            this.RemoveLine(this.drawingService.baseCtx, this.pathData); // to see in real time the  changes.
         }
     }
 
