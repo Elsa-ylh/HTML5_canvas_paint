f5b1226d3011e174f19cdf6343d83f7411f1ef29
Tai Chen Li <dev@taichen.ca>
2020-09-28 20:56:21 -0400

    continuation of color picker

--- client/src/app/services/color/color.service.ts
+++ client/src/app/services/color/color.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Vec2 } from '@app/classes/vec2';
 
+export interface RGB {
+    red: string;
+    green: string;
+    blue: string;
+}
+
 @Injectable({
     providedIn: 'root',
 })
 export class ColorService {
     constructor() {}
+    primaryColor: string;
+    secondaryColor: string;
 
     testMethod(): void {
         console.log("it's working");
@@ -32,5 +40,19 @@ export class ColorService {
         ctx.fillStyle = gradient;
         ctx.fill();
         ctx.closePath();
+    }
+
+    // Ce code est complètement inspiré sans gêne de
+    // https://malcoded.com/posts/angular-color-picker/#detecting-mouse-events-on-the-color-slider
+    getColor(position: Vec2, ctx: CanvasRenderingContext2D): void {
+        // const rgb = ctx.getImageData(position.x, position.y, 1, 1).data as Uint8ClampedArray;
+        // rgb[0];
+        // return { red: rgb};
+    }
+
+    swapColor(): void {
+        const temp = this.primaryColor;
+        this.primaryColor = this.secondaryColor;
+        this.secondaryColor = temp;
     }
 }
