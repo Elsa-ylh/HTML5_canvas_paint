5fc9274085c763540bf923600a7d24e58fe328ee
alphonse <alphonse.desrosiers@polymtl.ca>
2020-09-29 22:41:39 -0400

    fix lint erreur

--- client/src/app/classes/event-of-test.ts
+++ client/src/app/classes/event-of-test.ts
@@ -1,30 +1,29 @@
 import { MouseButton } from '@app/classes/mouse-button';
 export class EventOfTest {
-    constructor() {}
-    public mouseEvent = {
+    mouseEvent: MouseEvent = {
         offsetX: 25,
         offsetY: 10,
         button: MouseButton.Left,
     } as MouseEvent;
-    public mouseEvent1 = {
+    mouseEvent1: MouseEvent = {
         offsetX: 0,
         offsetY: 0,
         button: MouseButton.Left,
     } as MouseEvent;
-    public mouseEvent2 = {
+    mouseEvent2: MouseEvent = {
         offsetX: 50,
         offsetY: 50,
         button: MouseButton.Left,
     } as MouseEvent;
-    public mouseEvent3 = {
+    mouseEvent3: MouseEvent = {
         offsetX: 0,
         offsetY: 10,
         button: MouseButton.Left,
     } as MouseEvent;
-    public mouseEventR = {
+    mouseEventR: MouseEvent = {
         offsetX: 0,
         offsetY: 0,
         button: MouseButton.Right,
     } as MouseEvent;
-    public backspceEvant = new KeyboardEvent('backspace');
+    backspceEvant: KeyboardEvent = new KeyboardEvent('backspace');
 }
