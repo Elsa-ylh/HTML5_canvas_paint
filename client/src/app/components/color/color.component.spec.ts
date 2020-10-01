c66cf8d74958112df0a120b472ca9574356cb5b9
TheRealFlash <john.maliha@polymtl.ca>
2020-10-01 17:50:59 -0400

    Test and lint pass

--- client/src/app/components/color/color.component.spec.ts
+++ client/src/app/components/color/color.component.spec.ts
@@ -1,13 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+// import { ColorService } from '@app/services/color/color.service';
+// import { DrawingService } from '@app/services/drawing/drawing.service';
 import { ColorComponent } from './color.component';
 
 describe('ColorComponent', () => {
     let component: ColorComponent;
     let fixture: ComponentFixture<ColorComponent>;
+    // let colorStub: ColorService;
+    // let drawingStub: DrawingService;
 
     beforeEach(async () => {
+        // sanitizer = new DomSanitizer();
+        // colorStub = new ColorService(drawingStub);
+        // matDialog= new MatDialog;
         await TestBed.configureTestingModule({
             declarations: [ColorComponent],
+            providers: [
+                { provide: MatDialog, useValue: {} },
+                {
+                    provide: MatIconRegistry,
+                    useValue: {
+                        addSvgIcon: () => '',
+                    },
+                },
+                {
+                    provide: DomSanitizer,
+                    useValue: {
+                        bypassSecurityTrustResourceUrl: () => '',
+                    },
+                },
+            ],
         }).compileComponents();
     });
 
