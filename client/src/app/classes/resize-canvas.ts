5cf05c48bd867d4ebc61ca5d8cd8335cfdd34924
Tai Chen Li <dev@taichen.ca>
2020-10-01 19:13:27 -0400

    linting fix

--- client/src/app/classes/resize-canvas.ts
+++ client/src/app/classes/resize-canvas.ts
@@ -1,11 +1,14 @@
-export const MIN_CANVAS_SIZE: number = 250;
-export const WORK_AREA_PADDING_SIZE: number = 100;
-export const SIDEBAR_WIDTH: number = 226;
-export const ICON_WIDTH: number = 50;
+export const MIN_CANVAS_SIZE = 250;
+export const WORK_AREA_PADDING_SIZE = 100;
+export const SIDEBAR_WIDTH = 226;
+export const ICON_WIDTH = 50;
 
-export const RESIZE_MIDDLE_LOWER_PROPORTION: number = 1 / 3;
-export const RESIZE_MIDDLE_UPPER_PROPORTION: number = 2 / 3;
-export const RESIZE_CORNER_PROPORTION: number = 8 / 10;
-export const RESIZE_DASH_SPACING: number = 10;
-export const RESIZE_DASH_THICKNESS: number = 1;
-export const RESIZE_HOOK_THICKNESS: number = 30;
+// tslint:disable-next-line:no-magic-numbers
+export const RESIZE_MIDDLE_LOWER_PROPORTION = 1 / 3;
+// tslint:disable-next-line:no-magic-numbers
+export const RESIZE_MIDDLE_UPPER_PROPORTION = 2 / 3;
+// tslint:disable-next-line:no-magic-numbers
+export const RESIZE_CORNER_PROPORTION = 8 / 10;
+export const RESIZE_DASH_SPACING = 10;
+export const RESIZE_DASH_THICKNESS = 1;
+export const RESIZE_HOOK_THICKNESS = 30;
