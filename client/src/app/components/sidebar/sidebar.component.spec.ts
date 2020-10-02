c4da5e29be37fd88b6011151ca66584835aea8ee
TheRealFlash <john.maliha@polymtl.ca>
2020-10-01 23:24:20 -0400

    Color service talmost done

--- client/src/app/components/sidebar/sidebar.component.spec.ts
+++ client/src/app/components/sidebar/sidebar.component.spec.ts
@@ -2,21 +2,57 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
+import { SubToolselected } from '@app/classes/sub-tool-selected';
+import { Tool, ToolUsed } from '@app/classes/tool';
+import { ColorService } from '@app/services/color/color.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+import { ToolService } from '@app/services/tool-service';
+import { BrushService } from '@app/services/tools/brush.service';
+import { EllipseService } from '@app/services/tools/ellipse.service';
+import { EraserService } from '@app/services/tools/eraser-service';
+import { LineService } from '@app/services/tools/line.service';
+import { PencilService } from '@app/services/tools/pencil-service';
+import { RectangleService } from '@app/services/tools/rectangle.service';
 import { SidebarComponent } from './sidebar.component';
+
+class ToolStub extends Tool {}
 
 describe('SidebarComponent', () => {
     let component: SidebarComponent;
     let fixture: ComponentFixture<SidebarComponent>;
     let drawingStub: DrawingService;
-
+    let toolServiceStub: ToolService;
+    let toolStub: ToolStub;
+    let rectangleStub: RectangleService;
+    let ellipseStub: EllipseService;
+    let brushStub: BrushService;
+    let pencilStub: PencilService;
+    let eraserStub: EraserService;
+    let lineStub: LineService;
+    let ColorStub: ColorService;
     beforeEach(async () => {
         drawingStub = new DrawingService();
+        ColorStub = new ColorService(drawingStub);
+        // rectangleStub = new RectangleService(drawingStub, ColorStub);
+        // ellipseStub = new EllipseService(drawingStub, ColorStub);
+        brushStub = new BrushService(drawingStub);
+        pencilStub = new PencilService(drawingStub, ColorStub);
+        eraserStub = new EraserService(drawingStub);
+        lineStub = new LineService(drawingStub);
+        toolServiceStub = new ToolService(pencilStub, eraserStub, brushStub, lineStub, rectangleStub, ellipseStub);
 
         await TestBed.configureTestingModule({
             declarations: [SidebarComponent],
             providers: [
                 { provide: DrawingService, useValue: drawingStub },
+                { provide: ToolService, useValue: toolStub },
+                { provide: RectangleService, useValue: rectangleStub },
+                { provide: EllipseService, useValue: ellipseStub },
+                { provide: BrushService, useValue: brushStub },
+                { provide: PencilService, useValue: pencilStub },
+                { provide: EraserService, useValue: eraserStub },
+                { provide: LineService, useValue: lineStub },
+                { provide: ToolService, useValue: toolServiceStub },
                 { provide: MatDialog, useValue: {} },
                 {
                     provide: MatIconRegistry,
@@ -42,5 +78,46 @@ describe('SidebarComponent', () => {
 
     it('should create', () => {
         expect(component).toBeTruthy();
+    });
+    // it('createNew drawing ', () => {
+    //    component.createNewDrawing();
+    //    expect(component).toHaveBeenCalled();
+    // });
+    // it('clearCanvas', () => {
+    //    component.clearCanvas();
+    //    expect(component.isDialogOpen).toEqual(false);
+    // });
+    it('pickPencil', () => {
+        component.pickPencil();
+        expect(toolServiceStub.currentToolName).toEqual(ToolUsed.Pencil);
+    });
+    it('pickEraser()', () => {
+        component.pickEraser();
+        expect(toolServiceStub.currentToolName).toEqual(ToolUsed.Eraser);
+    });
+    // it('pickBrush()', () => {
+    //    component.pickBrush(SubToolselected.tool1);
+    //    expect(toolServiceStub.currentToolName).toEqual(ToolUsed.Brush);
+    // });
+    // it('pickLine()', () => {
+    //    component.pickLine();
+    //    expect(toolServiceStub.currentToolName).toEqual(ToolUsed.Line);
+    // });
+    it('pickRectangle()', () => {
+        component.pickRectangle(SubToolselected.tool1);
+        expect(toolServiceStub.currentToolName).toEqual(ToolUsed.Rectangle);
+    });
+    it('pickEllipse()', () => {
+        component.pickEllipse(SubToolselected.tool1);
+        expect(toolServiceStub.currentToolName).toEqual(ToolUsed.Ellipse);
+    });
+    /*it('pickColor()', () => {
+        component.pickColor();
+        expect(toolServiceStub.currentToolName).not.toEqual(ToolUsed.Pencil);
+    });*/
+    it('resetCheckedButton()', () => {
+        component.resetCheckedButton();
+        const isPencilChecked: boolean = component.pencilChecked;
+        expect(isPencilChecked).toBeFalse;
     });
 });
--- client/src/app/services/color/color.service.spec.ts
+++ client/src/app/services/color/color.service.spec.ts
@@ -3,26 +3,25 @@
 import { inject, TestBed } from '@angular/core/testing';
 import { canvasTestHelper } from '@app/classes/canvas-test-helper';
 import { EventOfTest } from '@app/classes/event-of-test';
+import { RGBA } from '@app/classes/rgba';
+// import { RGBA } from '@app/classes/rgba';
 import { ColorService } from '@app/services/color/color.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
+// tslint:disable:no-magic-numbers
 describe('ColorService', () => {
     let service: ColorService;
-    let DrawingService: DrawingService;
     let mouseEventDrawDot: EventOfTest;
 
     let baseCtxStub: CanvasRenderingContext2D;
     let previewCtxStub: CanvasRenderingContext2D;
-    let drawServiceSpy: jasmine.SpyObj<DrawingService>;
-    // tslint:disable:no-any
-    // let drawMovingDotSpy: jasmine.Spy<any>;
 
     beforeEach(() => {
         baseCtxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
         previewCtxStub = canvasTestHelper.drawCanvas.getContext('2d') as CanvasRenderingContext2D;
 
         TestBed.configureTestingModule({
-            providers: [{ provide: DrawingService, useValue: drawServiceSpy }],
+            providers: [{ provide: DrawingService }],
         });
 
         service = TestBed.inject(ColorService);
@@ -43,7 +42,7 @@ describe('ColorService', () => {
     it('getLastColor() and setLastColor() should get et set the color from lastColors ', () => {
         service.addLastColor('#FFFFFF');
         service.getlastColors();
-        expect(service.getlastColors()).toEqual();
+        
     });
     */
 
@@ -53,15 +52,41 @@ describe('ColorService', () => {
         expect(baseCtxStub.strokeStyle).toEqual('#000000');
         expect(baseCtxStub.fillStyle).toEqual('#ffffff');
     });
+    /*
+    it('getcolor() should read data from the canvas ', () => {
+        const imageData: RGBA = service.getColor({ x: 0, y: 0 }, baseCtxStub);
+        expect(imageData[0]).toEqual(0); // R
+        expect(imageData.[1]).toEqual(255); // G
+        expect(imageData.[2]).toEqual(255); // B
+        expect(imageData.[3]).toEqual(255); // A
+    });
+    */
+    it('changeColoOpacity should change the opacity of primary color ', () => {
+        service.isclicked = true;
+        service.changeColorOpacity(1);
+        expect(service.primaryColorTransparency).toEqual(1);
+        expect(baseCtxStub.globalAlpha).toEqual(1);
+    });
 
-    it('getcolor() should read data from the canvas '),
-        () => {
-            baseCtxStub.strokeStyle = 'black';
-            service.getColor({ x: 0, y: 0 }, baseCtxStub);
-            const imageData: ImageData = baseCtxStub.getImageData(0, 0, 1, 1);
-            expect(imageData.data[0]).toEqual(0); // R
-            expect(imageData.data[1]).toEqual(0); // G
-            expect(imageData.data[2]).toEqual(0); // B
-            expect(imageData.data[3]).toEqual(1); // A
-        };
+    it('changeColoOpacity should change the opacity of secondary color ', () => {
+        service.isclicked = false;
+        service.changeColorOpacity(1);
+        expect(service.secondaryColorTransparency).toEqual(1);
+        expect(baseCtxStub.globalAlpha).toEqual(1);
+    });
+
+    it('swapColor should invert primary and secondary colors ', () => {
+        service.primaryColor = '#FFFFFF';
+        service.secondaryColor = '#000000';
+        service.swapColor();
+        expect(service.primaryColor).toEqual('#000000');
+        expect(service.secondaryColor).toEqual('#FFFFFF');
+    });
+
+    it('numeralToHex() should return a hex value from a rgb ', () => {
+        const value: RGBA = { red: 255, green: 0, blue: 0, alpha: 1 };
+
+        service.numeralToHex(value);
+        expect(service.numeralToHex(value)).toEqual('#ff0000');
+    });
 });
