3328daaeea2fc050cc96d85bbd283f1ab63bca25
alphonse <alphonse.desrosiers@polymtl.ca>
2020-10-08 17:33:04 -0400

    fix error tsconfig.json

--- common/communication/canvas-information.ts
+++ common/communication/canvas-information.ts
@@ -2,8 +2,8 @@ export interface CancasInformation {
     _id: string;
     name: string;
     labels: Array<Label>;
-    image: string; //base64
     date: string;
+    image: string; //base64
 }
 export interface Label {
     label: string;
