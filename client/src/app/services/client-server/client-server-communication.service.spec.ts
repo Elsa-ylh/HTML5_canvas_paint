661e6a5548a65d6f393f755451e48f9fa7737b9d
alphonse <alphonse.desrosiers@polymtl.ca>
2020-10-13 17:45:26 -0400

    allo

--- client/src/app/services/client-server/client-server-communication.service.spec.ts
+++ client/src/app/services/client-server/client-server-communication.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { CancasInformation } from '@common/communication/canvas-information';
 import { ClientServerCommunicationService } from './client-server-communication.service';
 
 fdescribe('ClientServerCommunicationService', () => {
@@ -15,7 +16,7 @@ fdescribe('ClientServerCommunicationService', () => {
         });
         service = TestBed.inject(ClientServerCommunicationService);
         httpMock = TestBed.inject(HttpTestingController);
-        baseUrl = service.HTTP_SERVE_LOCAL;
+        baseUrl = service['HTTP_SERVE_LOCAL'];
     });
 
     afterEach(() => {
@@ -27,5 +28,19 @@ fdescribe('ClientServerCommunicationService', () => {
     });
     it('retour get url http', () => {
         console.log(service.getData());
+    });
+    it('should return expected message (HttpClient called once)', () => {
+        const expectedMessage: CancasInformation = { id: '', name: 'test5', labels: [{ label: 'label1' }], date: '2020-10-08', picture: 'test5' };
+
+        // check the content of the mocked call
+        service.getData().subscribe((response: CancasInformation) => {
+            expect(response.id).toEqual(expectedMessage.id, 'Title check');
+            expect(response.name).toEqual(expectedMessage.name, 'body check');
+        }, fail);
+
+        const req = httpMock.expectOne(baseUrl);
+        expect(req.request.method).toBe('GET');
+        // actually send the request
+        req.flush(expectedMessage);
     });
 });
