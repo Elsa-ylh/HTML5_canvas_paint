7770be9984be9ead8a6ab06db1402132e00aa611
Tai Chen Li <dev@taichen.ca>
2020-10-18 09:46:38 -0400

    continuation of export dialog

--- client/src/app/components/dialog-export-drawing/dialog-export-drawing.component.ts
+++ client/src/app/components/dialog-export-drawing/dialog-export-drawing.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Data } from '@angular/router';
 
@@ -8,5 +9,23 @@ import { Data } from '@angular/router';
     styleUrls: ['./dialog-export-drawing.component.scss'],
 })
 export class DialogExportDrawingComponent {
-    constructor(@Inject(MAT_DIALOG_DATA) public data: Data, public dialogRef: MatDialogRef<DialogExportDrawingComponent>) {}
+    isJPG = false;
+    isPNG = false;
+
+    // change the name of the filter when effect has been chosen
+    whichFilter = new Map([
+        ['filterOne', false],
+        ['filterTwo', false],
+        ['filterThree', false],
+        ['filterFour', false],
+        ['filterFive', false],
+    ]);
+
+    nameFormControl: FormGroup;
+
+    constructor(@Inject(MAT_DIALOG_DATA) private data: Data, private dialogRef: MatDialogRef<DialogExportDrawingComponent>, private fb: FormBuilder) {
+        this.nameFormControl = this.fb.group({
+            name: ['', Validators.required, Validators.minLength(1), Validators.maxLength(255)], // 255 is the max characters for certain operating system
+        });
+    }
 }
