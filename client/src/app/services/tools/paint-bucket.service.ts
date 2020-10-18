163655da333cdf7377815bface4fa08cd7ff365a
Lachieze-Rey Hugo <hugo.lachieze-rey@polymtl.ca>
2020-10-18 18:44:52 -0400

    add get pixel

--- client/src/app/services/tools/paint-bucket.service.ts
+++ client/src/app/services/tools/paint-bucket.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { MouseButton } from '@app/classes/mouse-button';
+import { ICON_WIDTH, SIDEBAR_WIDTH } from '@app/classes/resize-canvas';
+import { RGBA } from '@app/classes/rgba';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { ColorService } from '@app/services/color/color.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
+
+// https://github.com/williammalone/HTML5-Paint-Bucket-Tool/blob/master/html5-canvas-paint-bucket.js => paint bucket in js
 
 // The following pseudo code has been taken from this website : https://hackernoon.com/
 // floodFill(image[row][col], x, y, prevColor, newColor)
@@ -28,6 +32,12 @@ export class PaintBucketService extends Tool {
     mousePosition: Vec2;
     mouseEnter: boolean = false;
     mouseOut: boolean = false;
+    DEFAULT_WIDTH: number = (window.innerWidth - SIDEBAR_WIDTH - ICON_WIDTH) / 2;
+    DEFAULT_HEIGHT: number = window.innerHeight / 2;
+    canvasSize: Vec2 = { x: this.DEFAULT_WIDTH, y: this.DEFAULT_HEIGHT };
+    targetColor: RGBA;
+    // visited: Number = new Array(1)[1000];
+    colorAttributs: number = 4;
 
     constructor(drawingService: DrawingService, private colorService: ColorService) {
         super(drawingService);
@@ -48,11 +58,10 @@ export class PaintBucketService extends Tool {
         this.mousePosition = this.mouseDownCoord;
     }
 
-    // async onMouseDown(e: MouseEvent): Promise<void> {
-    //   await this.getCanvasData();
-    //   this.stack.push(new Point2D(e.offsetX, e.offsetY));
-    //   this.targetColor = this.getColor(new Point2D(e.offsetX, e.offsetY));
-    //   this.visited = [];
+    // onMouseDown(event: MouseEvent): void {
+    //   this.stack.push(new vec2(event.offsetX, event.offsetY));
+    //   this.targetColor = this.colorService.getColor(new vec2 (event.offsetX, event.offsetY), this.drawingService.baseCtx);
+
     //   this.control.addShape(this.shape);
     //   this.shape.style.strokeWidth = '1';
     //   this.shape.style.primaryColor = this.colors.getPrimaryString();
@@ -65,6 +74,7 @@ export class PaintBucketService extends Tool {
     //     if (point) { this.floodFill(point);}
     //     this.pathify();
     //   }
+    // }
 
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
@@ -101,6 +111,41 @@ export class PaintBucketService extends Tool {
         this.mouseOut = false;
     }
 
+    // cnt : number = 0;
+    // row : number;
+    // col : number;
+    // image : Number = new Array(2) [1000][1000];
+    // visited : Boolean = new Array(2) [1000][1000];
+    // res : Number = new Array(1) [1000];
+
+    // valid( i : number, j : number) : boolean
+    // {
+    //     if(i<0 || i>=this.row || j<0 || j>=this.col)
+    //         return false;
+    //     else
+    //         return true;
+    // }
+
+    // floodFill( i : number, j: number, idxValue : number)
+    // {
+    //     if(this.valid(i,j) == false)     //Base case
+    //         return;
+    //     if(this.image[i][j] != idxValue)
+    //         return;
+    //     if(this.image[i][j] == idxValue)
+    //         this.cnt++;
+    //     this.visited[i][j] = 1;               //Current node is marked as visited.
+    //     this.image[i][j] = -100;
+    //     this.floodFill(i-1,j,idxValue);
+    //     this.floodFill(i+1,j,idxValue);
+    //     this.floodFill(i,j-1,idxValue);
+    //     this.floodFill(i,j+1,idxValue);
+    //     this.floodFill(i-1,j-1,idxValue);
+    //     this.floodFill(i-1,j+1,idxValue);
+    //     this.floodFill(i+1,j-1,idxValue);
+    //     this.floodFill(i+1,j+1,idxValue);
+    // }
+
     // floodFill(pos: Point2D, stack: Point2D[]): void {
     //   if (this.stroke.includes(pos)) { return; }
     //   this.setColor(pos);
@@ -115,4 +160,17 @@ export class PaintBucketService extends Tool {
     //   const nextPoint = stack.pop();
     //   if (nextPoint) {this.floodFill(nextPoint, stack); }
     // }
+
+    // Get current pixel position
+    getPixel(position: Vec2, ctx: CanvasRenderingContext2D): RGBA {
+        const imageData = ctx.getImageData(0, 0, this.canvasSize.x, this.canvasSize.y).data;
+        if (position.x < 0 || position.y < 0 || this.canvasSize.x > 0 || this.canvasSize.y > 0) {
+            // tslint:disable-next-line:no-magic-numbers
+            return { red: imageData[-1], green: imageData[-1], blue: imageData[-1], alpha: 0 }; // impossible color
+        } else {
+            const offset = (position.y * this.canvasSize.x + position.x) * this.colorAttributs;
+            // tslint:disable-next-line:no-magic-numbers
+            return { red: imageData[offset], green: imageData[offset + 1], blue: imageData[offset + 2], alpha: imageData[offset + 3] };
+        }
+    }
 }
