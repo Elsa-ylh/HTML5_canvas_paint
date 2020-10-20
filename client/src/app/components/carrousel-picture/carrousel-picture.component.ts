767a5d01aebfa631c0179f04384ee2e0cfde0304
alphonse <alphonse.desrosiers@polymtl.ca>
2020-10-20 00:52:47 -0400

    most label si finish to html

--- client/src/app/components/carrousel-picture/carrousel-picture.component.ts
+++ client/src/app/components/carrousel-picture/carrousel-picture.component.ts
@@ -1,7 +1,7 @@
-// import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component } from '@angular/core';
 import { ClientServerCommunicationService } from '@app/services/client-server/client-server-communication.service';
 import { CancasInformation, Label } from '@common/communication/canvas-information';
+import { Message } from '@common/communication/message';
 
 @Component({
     selector: 'app-carrousel-picture',
@@ -11,10 +11,12 @@ import { CancasInformation, Label } from '@common/communication/canvas-informati
 export class CarrouselPictureComponent {
     private dataPicture: CancasInformation[];
     dataLabel: Label[];
+    private labelSelect: string[];
     // message: Message;
     constructor(private clientServerCommunicationService: ClientServerCommunicationService) {
         this.addAllData();
         this.addAllLabal();
+        this.labelSelect = [];
     }
 
     private addAllData(): void {
@@ -25,11 +27,36 @@ export class CarrouselPictureComponent {
         this.dataLabel = this.clientServerCommunicationService.getAllLabel();
     }
 
-    raffrachire(): void {
+    resete(): void {
         this.addAllLabal();
         this.addAllData();
+        this.labelSelect = [];
     }
-
+    refresh() {
+        this.addAllLabal();
+    }
+    selectionLabel(label: string) {
+        let itList: boolean = true;
+        for (let index = 0; index < this.labelSelect.length; index++) {
+            if (this.labelSelect[index] === label) {
+                this.labelSelect.splice(index, 1);
+                itList = false;
+            }
+        }
+        if (itList) {
+            this.labelSelect.push(label);
+        }
+        console.log(this.labelSelect.length);
+        this.labelSelect.length === 0 ? this.addAllData() : this.setMessageLabel(this.labelSelect);
+    }
+    private setMessageLabel(labels: string[]): void {
+        let textLabel: string = '';
+        for (let index = 0; index < labels.length; index++) {
+            textLabel += index === labels.length - 1 ? labels[index] : labels[index] + ',';
+        }
+        const message: Message = { title: 'labels', body: textLabel };
+        this.clientServerCommunicationService.selectPictureWithLabel(message).subscribe((info) => (this.dataPicture = info));
+    }
     getPicturesTestAll(): CancasInformation[] {
         return this.dataPicture;
     }
