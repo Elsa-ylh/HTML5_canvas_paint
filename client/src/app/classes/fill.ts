28deaa1c55b294c072cf74667edbc4f66775bda2
Lachieze-Rey Hugo <hugo.lachieze-rey@polymtl.ca>
2020-10-23 16:59:53 -0400

    add fill class for paint bucket

---
+++ client/src/app/classes/fill.ts
@@ -0,0 +1,137 @@
+import { CanvasResizerService } from '@app/services/canvas/canvas-resizer.service';
+import { ColorService } from '@app/services/color/color.service';
+import { DrawingService } from '@app/services/drawing/drawing.service';
+import { Point2d } from './point2d';
+import { Vec2 } from './vec2';
+
+export class Fill {
+    constructor(
+        ctx: CanvasRenderingContext2D,
+        point: Point2d,
+        color: string,
+        drawingService: DrawingService,
+        colorService: ColorService,
+        canvasResizerService: CanvasResizerService,
+    ) {
+        this.ctx = ctx;
+        this.point = point;
+        this.color = color;
+        this.imageData = this.drawingService.baseCtx.getImageData(
+            0,
+            0,
+            this.canvasResizerService.canvasSize.x,
+            this.canvasResizerService.canvasSize.y,
+        );
+        // const fillColor: string = this.hexToRgbA(color);
+        // const targetColor: number[] = this.getPixel(point as Vec2, drawingService.baseCtx);
+    }
+
+    ctx: CanvasRenderingContext2D;
+    point: Point2d;
+    color: string;
+    drawingService: DrawingService;
+    colorService: ColorService;
+    canvasResizerService: CanvasResizerService;
+    imageData: ImageData;
+    colorAttributs: number = 4;
+    pixelPos: number = 1;
+    radix: number = 16;
+    // tslint:disable-next-line: no-any
+    fillStack: any = [];
+    // fillColor: string = this.hexToRgbA(color);
+    // targetColor: number[] = this.getPixel(point as Vec2, drawingService.baseCtx);
+
+    floodFill(point: Vec2, targetColor: number[], fillColor: number[]): void {
+        if (this.colorMatch(targetColor, fillColor)) {
+            alert('same color');
+        }
+        const currentColor = this.getPixel(point as Vec2, this.drawingService.baseCtx);
+        if (this.colorMatch(currentColor, targetColor)) {
+            this.setPixel(point, fillColor);
+            this.fillStack.push([new Point2d(point.x + this.pixelPos, point.y)], targetColor, fillColor); // right side pixel
+            this.fillStack.push([new Point2d(point.x - this.pixelPos, point.y)], targetColor, fillColor); // left side pixel
+            this.fillStack.push([new Point2d(point.x, point.y + this.pixelPos)], targetColor, fillColor);
+            this.fillStack.push([new Point2d(point.x, point.y - this.pixelPos)], targetColor, fillColor);
+        }
+    }
+
+    getPixel(position: Vec2, ctx: CanvasRenderingContext2D): number[] {
+        if (position.x < 0 || position.y < 0 || this.imageData.width > 0 || this.imageData.height > 0) {
+            // return { red: this.imageData[-1], green: this.imageData[-1], blue: this.imageData[-1], alpha: 0 }; // impossible color
+            // tslint:disable-next-line:no-magic-numbers
+            return [-1, -1, -1, -1]; // impossible color
+        } else {
+            const offset = (position.y * this.imageData.width + position.x) * this.colorAttributs;
+            return [
+                this.imageData.data[offset + 0], // red
+                this.imageData.data[offset + 1], // green
+                this.imageData.data[offset + 2], // blue
+                // tslint:disable-next-line:no-magic-numbers
+                this.imageData.data[offset + 3], // alpha
+            ];
+        }
+    }
+
+    setPixel(point: Vec2, fillColor: number[]): void {
+        const offset = (point.y * this.imageData.width + point.x) * this.colorAttributs;
+        (this.imageData.data[offset + 0] = fillColor[0]), // red
+            (this.imageData.data[offset + 1] = fillColor[1]), // green
+            (this.imageData.data[offset + 2] = fillColor[2]), // blue
+            // tslint:disable-next-line:no-magic-numbers
+            (this.imageData.data[offset + 3] = fillColor[3]); // alpha
+    }
+
+    hexToRgbA(hex: string): number[] {
+        // let c: any;
+        // if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+        //     c = hex.substring(1).split('');
+        //     if (c.length == 3) {
+        //         c = [c[0], c[0], c[1], c[1], c[2], c[2]];
+        //     }
+        //     c = '0x' + c.join('');
+        //     return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',1)';
+        // }
+        // throw new Error('Bad Hex');
+        const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        if (result !== null) {
+            return [
+                // tslint:disable-next-line:no-non-null-assertion
+                parseInt(result![1], this.radix), // ! to assure that result is not null
+                // tslint:disable-next-line:no-non-null-assertion
+                parseInt(result![1], this.radix),
+                // tslint:disable-next-line:no-non-null-assertion
+                parseInt(result![1], this.radix),
+                // tslint:disable-next-line:no-magic-numbers
+                255,
+            ];
+        } else {
+            throw new Error('null hex');
+        }
+    }
+
+    fillForColor(): void {
+        if (this.fillStack.length()) {
+            const range = this.fillStack.length();
+            for (let i = 0; i < range; i++) {
+                this.floodFill(this.fillStack[0], this.fillStack[1], this.fillStack[2]);
+            }
+            this.fillStack.splice(0, range);
+            this.fillForColor();
+        } else {
+            // tslint:disable-next-line: no-any
+            this.drawingService.baseCtx.putImageData((this.imageData as any) as ImageData, 0, 0);
+            this.fillStack = [];
+        }
+    }
+
+    colorMatch(currentColor: number[], futurColor: number[]): boolean {
+        return (
+            currentColor[0] === futurColor[0] &&
+            currentColor[1] === futurColor[1] &&
+            // tslint:disable-next-line:no-magic-numbers
+            currentColor[2] === futurColor[2] &&
+            // tslint:disable-next-line:no-magic-numbers
+            currentColor[3] === futurColor[3]
+        );
+    }
+}
--- client/src/app/services/tools/paint-bucket.service.ts
+++ client/src/app/services/tools/paint-bucket.service.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Fill } from '@app/classes/fill';
 import { MouseButton } from '@app/classes/mouse-button';
-import { RGBA } from '@app/classes/rgba';
 import { Tool } from '@app/classes/tool';
 import { Vec2 } from '@app/classes/vec2';
 import { CanvasResizerService } from '@app/services/canvas/canvas-resizer.service';
@@ -26,30 +26,12 @@ import { DrawingService } from '@app/services/drawing/drawing.service';
 @Injectable({
     providedIn: 'root',
 })
-export class PaintBucketService extends Tool implements AfterViewInit {
+export class PaintBucketService extends Tool {
     fillColor: string;
     mousePosition: Vec2;
     mouseEnter: boolean = false;
     mouseOut: boolean = false;
-    // DEFAULT_WIDTH: number = (window.innerWidth - SIDEBAR_WIDTH - ICON_WIDTH) / 2;
-    // DEFAULT_HEIGHT: number = window.innerHeight / 2;
-    // canvasSize: Vec2 = { x: this.DEFAULT_WIDTH, y: this.DEFAULT_HEIGHT };
-    targetColor: RGBA;
-    // tslint:disable-next-line: no-any
-    fillStack: any = [];
-    colorAttribut: number = 4; // r, g, b, a => lint
-    // visited: Number = new Array(1)[1000];
-    colorAttributs: number = 4;
-
-    imageData: Uint8ClampedArray;
-    ngAfterViewInit(): void {
-        this.imageData = this.drawingService.baseCtx.getImageData(
-            0,
-            0,
-            this.canvasResizerService.canvasSize.x,
-            this.canvasResizerService.canvasSize.y,
-        ).data;
-    }
+    paintBucketTool: number = 8;
 
     constructor(drawingService: DrawingService, private colorService: ColorService, private canvasResizerService: CanvasResizerService) {
         super(drawingService);
@@ -63,30 +45,38 @@ export class PaintBucketService extends Tool implements AfterViewInit {
         if (this.mouseEnter) {
             this.onMouseUp(event);
         }
-        if (this.mouseDown) {
+        if (this.mouseDown && this.subToolSelect === this.paintBucketTool) {
             this.mouseDownCoord = this.getPositionFromMouse(event);
+            // tslint:disable-next-line:no-unused-expression
+            new Fill(
+                this.drawingService.baseCtx,
+                this.mouseDownCoord,
+                this.fillColor,
+                this.drawingService,
+                this.colorService,
+                this.canvasResizerService,
+            );
         }
-        // new Fill (this.drawingService.baseCtx, this.mouseDownCoord, this.fillColor)
         this.mousePosition = this.mouseDownCoord;
     }
 
-    //     // onMouseDown(event: MouseEvent): void {
-    //     //   this.stack.push(new vec2(event.offsetX, event.offsetY));
-    //     //   this.targetColor = this.colorService.getColor(new vec2 (event.offsetX, event.offsetY), this.drawingService.baseCtx);
+    // onMouseDown(event: MouseEvent): void {
+    //   this.fillStack.push(new Point2D(event.offsetX, event.offsetY));
+    //   this.targetColor = this.colorService.getColor((new Point2D (event.offsetX, event.offsetY)) as Vec2, this.drawingService.baseCtx);
 
-    //     //   this.control.addShape(this.shape);
-    //     //   this.shape.style.strokeWidth = '1';
-    //     //   this.shape.style.primaryColor = this.colors.getPrimaryString();
-    //     //   this.shape.style.secondaryColor = this.colors.getPrimaryString();
-    //     //   while (this.stack.length !== 0) {
-    //     //     console.log(this.stack.length);
-    //     //     let point = this.stack.pop();
-    //     //     while (point && this.visitedIncludes(point)) { point = this.stack.pop(); }
-    //     //     console.log(this.stack.length);
-    //     //     if (point) { this.floodFill(point);}
-    //     //     this.pathify();
-    //     //   }
-    //     // }
+    //   this.control.addShape(this.shape);
+    //   this.shape.style.strokeWidth = '1';
+    //   this.shape.style.primaryColor = this.colorService.primaryColor;
+    //   this.shape.style.secondaryColor = this.colorService.secondaryColor;
+    //   while (this.fillStack.length !== 0) {
+    //     console.log(this.fillStack.length);
+    //     let point = this.fillStack.pop();
+    //     while (point && this.visitedIncludes(point)) { point = this.fillStack.pop(); }
+    //     console.log(this.stack.length);
+    //     if (point) { this.floodFill(point);}
+    //     this.pathify();
+    //   }
+    // }
 
     onMouseUp(event: MouseEvent): void {
         if (this.mouseDown) {
@@ -120,119 +110,5 @@ export class PaintBucketService extends Tool implements AfterViewInit {
             this.mouseEnter = true;
         }
         this.mouseOut = false;
-    }
-
-    //     // cnt : number = 0;
-    //     // row : number;
-    //     // col : number;
-    //     // image : Number = new Array(2) [1000][1000];
-    //     // visited : Boolean = new Array(2) [1000][1000];
-    //     // res : Number = new Array(1) [1000];
-
-    //     // valid( i : number, j : number) : boolean
-    //     // {
-    //     //     if(i<0 || i>=this.row || j<0 || j>=this.col)
-    //     //         return false;
-    //     //     else
-    //     //         return true;
-    //     // }
-
-    //     // floodFill( i : number, j: number, idxValue : number)
-    //     // {
-    //     //     if(this.valid(i,j) == false)     //Base case
-    //     //         return;
-    //     //     if(this.image[i][j] != idxValue)
-    //     //         return;
-    //     //     if(this.image[i][j] == idxValue)
-    //     //         this.cnt++;
-    //     //     this.visited[i][j] = 1;               //Current node is marked as visited.
-    //     //     this.image[i][j] = -100;
-    //     //     this.floodFill(i-1,j,idxValue);
-    //     //     this.floodFill(i+1,j,idxValue);
-    //     //     this.floodFill(i,j-1,idxValue);
-    //     //     this.floodFill(i,j+1,idxValue);
-    //     //     this.floodFill(i-1,j-1,idxValue);
-    //     //     this.floodFill(i-1,j+1,idxValue);
-    //     //     this.floodFill(i+1,j-1,idxValue);
-    //     //     this.floodFill(i+1,j+1,idxValue);
-    //     // }
-
-    //     // floodFill(pos: Point2D, stack: Point2D[]): void {
-    //     //   if (this.stroke.includes(pos)) { return; }
-    //     //   this.setColor(pos);
-    //     //   this.stroke.push(pos);
-    //     //   const cardinalPoints: Point2D[] = [{x: pos.x, y: pos.y - 1}, {x: pos.x + 1, y: pos.y},
-    //     //                                      {x: pos.x, y: pos.y + 1}, {x: pos.x - 1, y: pos.y}];
-    //     //   cardinalPoints.forEach( (point) => {
-    //     //     const color = this.getColor(point);
-    //     //     if (this.toleranceCheck(color, this.targetColor) && !stack.includes(point)) { stack.push(point); }
-    //     //   });
-    //     //   console.log(stack.length);
-    //     //   const nextPoint = stack.pop();
-    //     //   if (nextPoint) {this.floodFill(nextPoint, stack); }
-    //     // }
-
-    floodFill(point: Vec2, targetColor: string, fillColor: string): void {
-        if (this.colorMatch(targetColor, fillColor)) {
-            // return;
-        }
-        const currentColor = this.getPixel(point, this.drawingService.baseCtx);
-        if (this.colorMatch(this.colorService.numeralToHex(currentColor), targetColor)) {
-            this.setPixel(point, fillColor);
-            this.fillStack.push([point.x + 1, point.y], targetColor, fillColor); // right side pixel
-            this.fillStack.push([point.x - 1, point.y], targetColor, fillColor); // left side pixel
-            this.fillStack.push([point.x, point.y + 1], targetColor, fillColor);
-            this.fillStack.push([point.x, point.y - 1], targetColor, fillColor);
-        }
-    }
-
-    fillForColor(): void {
-        if (this.fillStack.length()) {
-            const range = this.fillStack.length();
-            for (let i = 0; i < range; i++) {
-                this.floodFill(this.fillStack[0], this.fillStack[1], this.fillStack[2]);
-            }
-            this.fillStack.splice(0, range);
-            this.fillForColor();
-        } else {
-            // tslint:disable-next-line: no-any
-            this.drawingService.baseCtx.putImageData((this.imageData as any) as ImageData, 0, 0);
-            this.fillStack = [];
-        }
-    }
-
-    colorMatch(currentColor: string, futurColor: string): boolean {
-        return (
-            currentColor[0] === futurColor[0] &&
-            currentColor[1] === futurColor[1] &&
-            // tslint:disable-next-line:no-magic-numbers
-            currentColor[2] === futurColor[2] &&
-            // tslint:disable-next-line:no-magic-numbers
-            currentColor[3] === futurColor[3]
-        );
-    }
-
-    // Get current pixel position
-    getPixel(position: Vec2, ctx: CanvasRenderingContext2D): RGBA {
-        if (position.x < 0 || position.y < 0 || this.canvasResizerService.canvasSize.x > 0 || this.canvasResizerService.canvasSize.y > 0) {
-            // tslint:disable-next-line:no-magic-numbers
-            return { red: this.imageData[-1], green: this.imageData[-1], blue: this.imageData[-1], alpha: 0 }; // impossible color
-        } else {
-            const offset = (position.y * this.canvasResizerService.canvasSize.x + position.x) * this.colorAttributs;
-            return {
-                red: this.imageData[offset],
-                green: this.imageData[offset + 1],
-                blue: this.imageData[offset + 2],
-                // tslint:disable-next-line:no-magic-numbers
-                alpha: this.imageData[offset + 3],
-            };
-        }
-    }
-
-    setPixel(point: Vec2, fillColor: string): void {
-        const offset = (point.y * this.canvasResizerService.canvasSize.x + point.x) * this.colorAttributs;
-        for (let i = 0; i < this.colorAttribut; i++) {
-            ((this.imageData[offset + i] as unknown) as string) = fillColor[i]; // for rgba
-        }
     }
 }
