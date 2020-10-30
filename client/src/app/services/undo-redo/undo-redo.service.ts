ee00ec6f2c06ebcf8f51c00789ba19e3a92d8d45
TheRealFlash <john.maliha@polymtl.ca>
2020-10-30 14:08:47 -0400

    fixed bug of tai

--- client/src/app/services/undo-redo/undo-redo.service.ts
+++ client/src/app/services/undo-redo/undo-redo.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AbsUndoRedo } from '@app/classes/undo-redo/abs-undo-redo';
-import { ResizeCanvasAction } from '@app/classes/undo-redo/resize-Canvas-Action';
+// import { ResizeCanvasAction } from '@app/classes/undo-redo/resize-Canvas-Action';
 import { DrawingService } from '@app/services/drawing/drawing.service';
-import { CanvasResizerService } from '../canvas/canvas-resizer.service';
+// import { CanvasResizerService } from '../canvas/canvas-resizer.service';
 
 @Injectable({
     providedIn: 'root',
@@ -13,7 +13,8 @@ export class UndoRedoService {
     isDefaultCanvasResizeInStack: boolean = false;
     private listUndo: AbsUndoRedo[] = [];
     private listRedo: AbsUndoRedo[] = [];
-    constructor(private drawingService: DrawingService, private cvsResizerService: CanvasResizerService) {}
+    constructor(private drawingService: DrawingService) {}
+    // private cvsResizerService: CanvasResizerService
     // Controls the buttons of redo-undo
     onMouseUpActivateUndo(mouseEvent: MouseEvent): void {
         // there is one element
@@ -61,13 +62,13 @@ export class UndoRedoService {
         console.log('stack undo redo', this.listUndo);
     }
 
-    addUndo(resizeAction: ResizeCanvasAction): void {
-        if (!this.isDefaultCanvasResizeInStack) {
-            const undoRedoEvent = { offsetX: this.cvsResizerService.canvasSize.x, offsetY: this.cvsResizerService.canvasSize.y } as MouseEvent;
-            const resizeCanvasAction = new ResizeCanvasAction(undoRedoEvent, resizeCtx, baseCanvas, this.resizeDirection, this);
-            this.undoRedoService.addUndo(resizeCanvasAction);
-        }
-    }
+    // addUndo(resizeAction: ResizeCanvasAction): void {
+    //     if (!this.isDefaultCanvasResizeInStack) {
+    //         const undoRedoEvent = { offsetX: this.cvsResizerService.canvasSize.x, offsetY: this.cvsResizerService.canvasSize.y } as MouseEvent;
+    //         const resizeCanvasAction = new ResizeCanvasAction(undoRedoEvent, resizeCtx, baseCanvas, this.resizeDirection, this);
+    //         this.undoRedoService.addUndo(resizeCanvasAction);
+    //     }
+    // }
 
     // function that cancels the lastest modification.(ctrl z) we push the lastest element removed from the undo stack.
     undo(): void {
