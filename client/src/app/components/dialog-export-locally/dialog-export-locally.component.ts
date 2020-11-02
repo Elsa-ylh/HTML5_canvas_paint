23879f071bcbfc8193cf7c693aa680ba0cf5fb95
Tai Chen Li <dev@taichen.ca>
2020-11-02 00:58:59 -0500

    continuation of resizing picture before preview

--- client/src/app/components/dialog-export-locally/dialog-export-locally.component.ts
+++ client/src/app/components/dialog-export-locally/dialog-export-locally.component.ts
@@ -1,14 +1,15 @@
-import { Component, Inject } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Data } from '@angular/router';
+import { Vec2 } from '@app/classes/vec2';
 
 @Component({
     selector: 'app-dialog-export-locally',
     templateUrl: './dialog-export-locally.component.html',
     styleUrls: ['./dialog-export-locally.component.scss'],
 })
-export class DialogExportDrawingComponent {
+export class DialogExportDrawingComponent implements AfterViewInit {
     isJPG = false;
     isPNG = false;
 
@@ -22,8 +23,21 @@ export class DialogExportDrawingComponent {
     ]);
 
     nameFormControl: FormControl;
+    private imgReducerCanvas: HTMLCanvasElement;
+    private imgReducerCtx: CanvasRenderingContext2D;
+    private previewImgSize: Vec2 = { x: 300, y: 300 } as Vec2;
 
     constructor(@Inject(MAT_DIALOG_DATA) private data: Data, private dialogRef: MatDialogRef<DialogExportDrawingComponent>) {
         this.nameFormControl = new FormControl('default', Validators.pattern('[a-zA-Z ]*'));
+        this.imgReducerCanvas = document.createElement('canvas');
+        this.imgReducerCtx = this.imgReducerCanvas.getContext('2d') as CanvasRenderingContext2D;
+    }
+
+    @ViewChild('previewImage', { static: false }) previewImage: ElementRef<HTMLImageElement>;
+
+    // https://stackoverflow.com/questions/19262141/resize-image-with-javascript-canvas-smoothly
+    ngAfterViewInit(): void {
+        this.imgReducerCtx.drawImage();
+        this.previewImage.nativeElement.src = 'assets/leaf.svg';
     }
 }
