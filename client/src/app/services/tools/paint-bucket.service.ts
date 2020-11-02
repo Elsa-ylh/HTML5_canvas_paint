ad8bc513eec3932a7545feb37340e1b9908f6c61
Lachieze-Rey Hugo <hugo.lachieze-rey@polymtl.ca>
2020-11-02 16:33:35 -0500

    adding tolerance and fill with right mouse button

--- client/src/app/services/tools/paint-bucket.service.ts
+++ client/src/app/services/tools/paint-bucket.service.ts
@@ -7,6 +7,8 @@ import { CanvasResizerService } from '@app/services/canvas/canvas-resizer.servic
 import { ColorService } from '@app/services/color/color.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
 
+const MAX_TOLERANCE = 100;
+
 @Injectable({
     providedIn: 'root',
 })
@@ -19,30 +21,28 @@ export class PaintBucketService extends Tool {
     oldColor: RGBA;
     mouseOut: boolean = false;
     tolerance: number;
-    pixels: ImageData;
 
     // https://en.wikipedia.org/wiki/Flood_fill#:~:text=Flood%20fill%2C%20also%20called%20seed,in%20a%20multi%2Ddimensional%20array.
     // https://ben.akrin.com/?p=7888
     // https://ben.akrin.com/canvas_fill/fill_04.html
 
     /*tslint:disable:cyclomatic-complexity*/
-    floodFill(pos: Vec2, replacementColor: RGBA, tolerance: number): void {
-        this.pixels = this.drawingService.baseCtx.getImageData(0, 0, this.cvsResizerService.canvasSize.x, this.cvsResizerService.canvasSize.y);
+    floodFill(x: number, y: number, replacementColor: RGBA): void {
         const pixelStack: Vec2[] = [];
-        pixelStack.push(pos);
-
-        // Maximum tolerance of 100, Default to 0
-        // tslint:disable-next-line:no-magic-numbers
-        tolerance = !isNaN(this.tolerance) ? Math.min(Math.abs(Math.round(this.tolerance)), 100) : 0;
-        // console.log(tolerance);
-
-        let linearCords: number = (pos.y * this.cvsResizerService.canvasSize.x + pos.x) * this.colorAttributs;
+        pixelStack.push({ x, y });
+        const pixels: ImageData = this.drawingService.baseCtx.getImageData(
+            0,
+            0,
+            this.cvsResizerService.canvasSize.x,
+            this.cvsResizerService.canvasSize.y,
+        );
+        let linearCords: number = (y * this.cvsResizerService.canvasSize.x + x) * this.colorAttributs;
         const originalColor = {
-            red: this.pixels.data[linearCords],
-            green: this.pixels.data[linearCords + 1],
-            blue: this.pixels.data[linearCords + 2],
+            red: pixels.data[linearCords],
+            green: pixels.data[linearCords + 1],
+            blue: pixels.data[linearCords + 2],
             // tslint:disable-next-line:no-magic-numbers
-            alpha: this.pixels.data[linearCords + 3],
+            alpha: pixels.data[linearCords + 3],
         };
 
         // if current pixel matches clicked color
@@ -50,51 +50,51 @@ export class PaintBucketService extends Tool {
 
         while (pixelStack.length > 0) {
             const newPixel: Vec2 = pixelStack.shift() as Vec2;
-            pos.x = newPixel.x;
-            pos.y = newPixel.y;
+            x = newPixel.x;
+            y = newPixel.y;
 
             // console.log( x + ", " + y ) ;
 
-            linearCords = (pos.y * this.cvsResizerService.canvasSize.x + pos.x) * this.colorAttributs;
+            linearCords = (y * this.cvsResizerService.canvasSize.x + x) * this.colorAttributs;
             while (
-                pos.y-- >= 0 &&
-                this.pixels.data[linearCords] === originalColor.red &&
-                this.pixels.data[linearCords + 1] === originalColor.green &&
-                this.pixels.data[linearCords + 2] === originalColor.blue &&
+                y-- >= 0 &&
+                pixels.data[linearCords] === originalColor.red &&
+                pixels.data[linearCords + 1] === originalColor.green &&
+                pixels.data[linearCords + 2] === originalColor.blue &&
                 // tslint:disable-next-line:no-magic-numbers
-                this.pixels.data[linearCords + 3] === originalColor.alpha
+                pixels.data[linearCords + 3] === originalColor.alpha
             ) {
                 linearCords -= this.cvsResizerService.canvasSize.x * this.colorAttributs;
             }
             linearCords += this.cvsResizerService.canvasSize.x * this.colorAttributs;
-            pos.y++;
+            y++;
 
             let reachedLeft = false;
             let reachedRight = false;
             while (
-                pos.y++ < this.cvsResizerService.canvasSize.y &&
-                this.pixels.data[linearCords] === originalColor.red &&
-                this.pixels.data[linearCords + 1] === originalColor.green &&
-                this.pixels.data[linearCords + 2] === originalColor.blue &&
+                y++ < this.cvsResizerService.canvasSize.y &&
+                pixels.data[linearCords] === originalColor.red &&
+                pixels.data[linearCords + 1] === originalColor.green &&
+                pixels.data[linearCords + 2] === originalColor.blue &&
                 // tslint:disable-next-line:no-magic-numbers
-                this.pixels.data[linearCords + 3] === originalColor.alpha
+                pixels.data[linearCords + 3] === originalColor.alpha
             ) {
-                this.pixels.data[linearCords] = replacementColor.red;
-                this.pixels.data[linearCords + 1] = replacementColor.green;
-                this.pixels.data[linearCords + 2] = replacementColor.blue;
+                pixels.data[linearCords] = replacementColor.red;
+                pixels.data[linearCords + 1] = replacementColor.green;
+                pixels.data[linearCords + 2] = replacementColor.blue;
                 // tslint:disable-next-line:no-magic-numbers
-                this.pixels.data[linearCords + 3] = replacementColor.alpha;
+                pixels.data[linearCords + 3] = replacementColor.alpha;
 
-                if (pos.x > 0) {
+                if (x > 0) {
                     if (
-                        this.pixels.data[linearCords - this.colorAttributs] === originalColor.red &&
-                        this.pixels.data[linearCords - this.colorAttributs + 1] === originalColor.green &&
-                        this.pixels.data[linearCords - this.colorAttributs + 2] === originalColor.blue &&
+                        pixels.data[linearCords - this.colorAttributs] === originalColor.red &&
+                        pixels.data[linearCords - this.colorAttributs + 1] === originalColor.green &&
+                        pixels.data[linearCords - this.colorAttributs + 2] === originalColor.blue &&
                         // tslint:disable-next-line:no-magic-numbers
-                        this.pixels.data[linearCords - this.colorAttributs + 3] === originalColor.alpha
+                        pixels.data[linearCords - this.colorAttributs + 3] === originalColor.alpha
                     ) {
                         if (!reachedLeft) {
-                            pixelStack.push({ x: pos.x - 1, y: pos.y });
+                            pixelStack.push({ x: x - 1, y });
                             reachedLeft = true;
                         }
                     } else if (reachedLeft) {
@@ -102,16 +102,16 @@ export class PaintBucketService extends Tool {
                     }
                 }
 
-                if (pos.x < this.cvsResizerService.canvasSize.x - 1) {
+                if (x < this.cvsResizerService.canvasSize.x - 1) {
                     if (
-                        this.pixels.data[linearCords + this.colorAttributs] === originalColor.red &&
-                        this.pixels.data[linearCords + this.colorAttributs + 1] === originalColor.green &&
-                        this.pixels.data[linearCords + this.colorAttributs + 2] === originalColor.blue &&
+                        pixels.data[linearCords + this.colorAttributs] === originalColor.red &&
+                        pixels.data[linearCords + this.colorAttributs + 1] === originalColor.green &&
+                        pixels.data[linearCords + this.colorAttributs + 2] === originalColor.blue &&
                         // tslint:disable-next-line:no-magic-numbers
-                        this.pixels.data[linearCords + this.colorAttributs + 3] === originalColor.alpha
+                        pixels.data[linearCords + this.colorAttributs + 3] === originalColor.alpha
                     ) {
                         if (!reachedRight) {
-                            pixelStack.push({ x: pos.x + 1, y: pos.y });
+                            pixelStack.push({ x: x + 1, y });
                             reachedRight = true;
                         }
                     } else if (reachedRight) {
@@ -122,26 +122,11 @@ export class PaintBucketService extends Tool {
                 linearCords += this.cvsResizerService.canvasSize.x * this.colorAttributs;
             }
         }
-        this.drawingService.baseCtx.putImageData(this.pixels, 0, 0);
+        this.drawingService.baseCtx.putImageData(pixels, 0, 0);
     }
     /*tslint:enable:cyclomatic-complexity*/
 
     // clic droit : double for dans une nouvelle fonction fill
-
-    fill(pos: Vec2, replacementColor: RGBA, tolerance: number): void {
-        this.pixels = this.drawingService.baseCtx.getImageData(0, 0, this.cvsResizerService.canvasSize.x, this.cvsResizerService.canvasSize.y);
-        const linearCords: number = (pos.y * this.cvsResizerService.canvasSize.x + pos.x) * this.colorAttributs;
-
-        for (let i = 0; i < this.cvsResizerService.canvasSize.x; i++) {
-            for (let j = 0; j < this.cvsResizerService.canvasSize.y; j++) {
-                this.pixels.data[linearCords] = replacementColor.red;
-                this.pixels.data[linearCords + 1] = replacementColor.green;
-                this.pixels.data[linearCords + 2] = replacementColor.blue;
-                // tslint:disable-next-line:no-magic-numbers
-                this.pixels.data[linearCords + 3] = replacementColor.alpha;
-            }
-        }
-    }
     // tolerance : nombre entre 0 et 100 => 0 tolerance max et 100 tolerance minimale => fill tout
     // difference entre les couleurs : red, blue, green, alpha puis moyenne a comparer => convertir en %
 
@@ -160,74 +145,74 @@ export class PaintBucketService extends Tool {
     }
 
     matchFillColor(currentColor: RGBA, targetColor: RGBA): boolean {
-        if (
-            currentColor.red === targetColor.red &&
-            currentColor.green === targetColor.green &&
-            currentColor.blue === targetColor.blue &&
-            currentColor.alpha === targetColor.alpha
-        ) {
-            return true;
-        } else {
-            return false;
+        let matchFillColor = true;
+        const tolerance = this.toleranceToRGBA();
+        console.log(tolerance);
+        matchFillColor = matchFillColor && targetColor.red >= currentColor.red - tolerance && targetColor.red <= currentColor.red + tolerance;
+        matchFillColor = matchFillColor && targetColor.green >= currentColor.green - tolerance && targetColor.green <= currentColor.green + tolerance;
+        matchFillColor = matchFillColor && targetColor.blue >= currentColor.blue - tolerance && targetColor.blue <= currentColor.blue + tolerance;
+
+        return matchFillColor;
+    }
+
+    fill(x: number, y: number, replacementColor: RGBA): void {
+        const pixels: ImageData = this.drawingService.baseCtx.getImageData(
+            0,
+            0,
+            this.cvsResizerService.canvasSize.x,
+            this.cvsResizerService.canvasSize.y,
+        );
+        const linearCords: number = (y * this.cvsResizerService.canvasSize.x + x) * this.colorAttributs;
+        const originalColor = {
+            red: pixels.data[linearCords],
+            green: pixels.data[linearCords + 1],
+            blue: pixels.data[linearCords + 2],
+            // tslint:disable-next-line:no-magic-numbers
+            alpha: pixels.data[linearCords + 3],
+        };
+        // tslint:disable-next-line:prefer-for-of
+        for (let i = 0; i < pixels.data.length; i++) {
+            // for (let j = 0; j < this.cvsResizerService.canvasSize.y; j++) {
+            if (this.matchFillColor(originalColor, replacementColor)) {
+                this.PaintPixel(
+                    this.drawingService.baseCtx.getImageData(0, 0, this.cvsResizerService.canvasSize.x, this.cvsResizerService.canvasSize.y),
+                    (y * this.cvsResizerService.canvasSize.x + x) * this.colorAttributs,
+                );
+            }
+            // }
         }
     }
 
-    PixelCompareAndSet(pos: Vec2, targetColor: RGBA, fillColor: RGBA, tolerance: number): boolean {
-        this.pixels = this.drawingService.baseCtx.getImageData(0, 0, this.cvsResizerService.canvasSize.x, this.cvsResizerService.canvasSize.y);
-        const linearCords: number = (pos.y * this.cvsResizerService.canvasSize.x + pos.x) * this.colorAttributs;
-        if (this.pixelCompare(pos, targetColor, fillColor, tolerance)) {
-            this.pixels.data[linearCords] = fillColor.red;
-            this.pixels.data[linearCords + 1] = fillColor.green;
-            this.pixels.data[linearCords + 2] = fillColor.blue;
-            // tslint:disable-next-line:no-magic-numbers
-            this.pixels.data[linearCords + 3] = fillColor.alpha;
-            return true;
-        }
-        return false;
+    PaintPixel(imageData: ImageData, pos: number): void {
+        const replacementColor: RGBA = this.hexToRgbA(this.colorService.primaryColor);
+        imageData.data[pos] = replacementColor.red;
+        imageData.data[pos + 1] = replacementColor.green;
+        imageData.data[pos + 2] = replacementColor.blue;
+        // tslint:disable-next-line:no-magic-numbers
+        imageData.data[pos + 3] = replacementColor.alpha;
     }
 
-    pixelCompare(pos: Vec2, originalColor: RGBA, replacementColor: RGBA, tolerance: number): boolean {
-        this.pixels = this.drawingService.baseCtx.getImageData(0, 0, this.cvsResizerService.canvasSize.x, this.cvsResizerService.canvasSize.y);
-        const linearCords: number = (pos.y * this.cvsResizerService.canvasSize.x + pos.x) * this.colorAttributs;
-        if (
-            Math.abs(originalColor.alpha - replacementColor.alpha) <= this.tolerance &&
-            Math.abs(originalColor.red - replacementColor.red) <= this.tolerance &&
-            Math.abs(originalColor.green - replacementColor.green) <= this.tolerance &&
-            Math.abs(originalColor.blue - replacementColor.blue) <= this.tolerance
-        )
-            return false; // target is same as fill
-
-        if (
-            // tslint:disable-next-line:no-magic-numbers
-            originalColor.alpha === this.pixels.data[linearCords + 3] &&
-            originalColor.red === this.pixels.data[linearCords] &&
-            originalColor.green === this.pixels.data[linearCords + 1] &&
-            originalColor.blue === this.pixels.data[linearCords + 2]
-        )
-            return true; // target matches surface
-
-        if (
-            // tslint:disable-next-line:no-magic-numbers
-            Math.abs(originalColor.alpha - this.pixels.data[linearCords + 3]) <= 255 - this.tolerance &&
-            Math.abs(originalColor.red - this.pixels.data[linearCords]) <= this.tolerance &&
-            Math.abs(originalColor.green - this.pixels.data[linearCords + 1]) <= this.tolerance &&
-            Math.abs(originalColor.blue - this.pixels.data[linearCords + 2]) <= this.tolerance
-        )
-            return true; // target to surface within tolerance
-
-        return false; // no match
+    toleranceToRGBA(): number {
+        // Maximum tolerance of 100, Default to 0
+        // tslint:disable-next-line:no-magic-numbers
+        this.tolerance = !isNaN(this.tolerance) ? Math.min(Math.abs(Math.round(this.tolerance)), 100) : 0;
+        // console.log('this : ', this.tolerance);
+        // tslint:disable-next-line:no-magic-numbers
+        return (this.tolerance / MAX_TOLERANCE) * 255;
     }
 
     onMouseDown(event: MouseEvent): void {
+        // debugger;
+        this.floodFill(event.offsetX, event.offsetY, this.hexToRgbA(this.colorService.primaryColor));
         // pixels contigus
         if (event.button === MouseButton.Left) {
             this.mouseDown = false;
-            this.floodFill({ x: event.offsetX, y: event.offsetY }, this.hexToRgbA(this.colorService.primaryColor), this.tolerance);
+            this.floodFill(event.offsetX, event.offsetY, this.hexToRgbA(this.colorService.primaryColor));
         }
         // pixels non contigus
         if (event.button === MouseButton.Right) {
             this.mouseDown = false;
-            this.fill({ x: event.offsetX, y: event.offsetY }, this.hexToRgbA(this.colorService.primaryColor), this.tolerance);
+            this.fill(event.offsetX, event.offsetY, this.hexToRgbA(this.colorService.primaryColor));
         }
     }
 
