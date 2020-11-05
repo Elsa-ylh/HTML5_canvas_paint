1a63387867b69426f42b2304b9c446a425360761
Tai Chen Li <dev@taichen.ca>
2020-11-04 22:33:17 -0500

    refactoring still working

--- server/app/services/index.service.ts
+++ server/app/services/index.service.ts
@@ -1,15 +1,5 @@
-import { Message } from '@common/communication/message';
 import { injectable } from 'inversify';
 import 'reflect-metadata';
 
 @injectable()
-export class IndexService {
-    constructor() {}
-
-    about(): Message {
-        return {
-            title: 'Basic Server About Page',
-            body: 'Try calling helloWorld to get the time',
-        };
-    }
-}
+export class IndexService {}
