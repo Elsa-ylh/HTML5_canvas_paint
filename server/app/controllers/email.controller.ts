aefef0d8dac6769ce2e4bc502f07f99a83054b1b
Tai Chen Li <dev@taichen.ca>
2020-11-15 22:58:25 -0500

    finally a working axios request

--- server/app/controllers/email.controller.ts
+++ server/app/controllers/email.controller.ts
@@ -1,4 +1,5 @@
 import { EmailService } from '@app/services/email.service';
+import axios, { AxiosResponse } from 'axios';
 import { Request, Response, Router } from 'express';
 import { inject, injectable } from 'inversify';
 import { TYPES } from '../types';
@@ -10,13 +11,28 @@ export class EmailController {
 
     constructor(@inject(TYPES.EmailService) private emailService: EmailService) {
         this.configureRouter();
-        const FormData = require('form-data');
-        const fs = require('fs');
-        const data = new FormData();
-        data.append('to', 'lithai357@gmail.com');
-        data.append('payload', fs.createReadStream('D:\\Downloads\\hellokl.jpg'));
 
-        this.emailService.sendEmail(data);
+        var FormData = require('form-data');
+        var fs = require('fs');
+        var data = new FormData();
+        data.append('to', 'lithai357@gmail.com');
+        data.append('payload', fs.createReadStream('D:/Desktop/yolo.png'));
+
+        axios({
+            method: 'post',
+            url: 'http://log2990.step.polymtl.ca/email?address_validation\n=true&quick_return=true&dry_run=false',
+            headers: {
+                'x-team-key': '42e98715-06d2-4f68-a853-e3fa5f7f9151',
+                ...data.getHeaders(),
+            },
+            data: data,
+        })
+            .then((response: AxiosResponse) => {
+                console.log(response);
+            })
+            .catch((error: Error) => {
+                console.log(error);
+            });
     }
     private configureRouter(): void {
         this.router = Router();
--- server/app/services/email.service.ts
+++ server/app/services/email.service.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
+import { ReadStream } from 'fs';
 import { injectable } from 'inversify';
 import 'reflect-metadata';
 
 @injectable()
 export class EmailService {
     private readonly x_team_key = '42e98715-06d2-4f68-a853-e3fa5f7f9151';
-    private readonly url = 'http://log2990.step.polymtl.ca/email?address_validation\n=true&quick_return=true&dry_run=false';
 
     // https://regexr.com/3e48o
     private readonly emailRegexValidation = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
@@ -19,15 +19,18 @@ export class EmailService {
         });
     }
 
-    async sendEmail(emailAndBinaryImage: FormData): Promise<boolean> {
+    async sendEmail(binaryImage: ReadStream): Promise<boolean> {
         return new Promise(() => {
             axios({
                 method: 'post',
-                url: this.url,
+                url: 'http://log2990.step.polymtl.ca/email?address_validation\n=true&quick_return=true&dry_run=false',
                 headers: {
                     'x-team-key': this.x_team_key,
                 },
-                data: emailAndBinaryImage,
+                data: {
+                    to: 'lithai357@gmail.com',
+                    payload: binaryImage.pipe,
+                },
             })
                 .then((response) => {
                     console.log(JSON.stringify(response.data));
