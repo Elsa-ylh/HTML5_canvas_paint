a513d1d32850ed3b8074520a7ed780fe92802606
Victor <victorkim@hotmail.ca>
2020-11-21 21:51:24 -0500

    mouseDown test added

--- client/src/app/services/tools/spray.service.spec.ts
+++ client/src/app/services/tools/spray.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { canvasTestHelper } from '@app/classes/canvas-test-helper';
 import { ColorService } from '@app/services/color/color.service';
 import { DrawingService } from '@app/services/drawing/drawing.service';
-// import { canvasTestHelper } from '@app/classes/canvas-test-helper';
 import { SprayService } from '@app/services/tools/spray.service';
 
 // tslint:disable:no-any
@@ -9,15 +9,16 @@ import { SprayService } from '@app/services/tools/spray.service';
 
 describe('SprayService', () => {
     let sprayService: SprayService;
-    // let baseStubCtx: CanvasRenderingContext2D;
+    let baseStubCtx: CanvasRenderingContext2D;
     // let previewStubCtx: CanvasRenderingContext2D;
     let colorServiceMock: jasmine.SpyObj<ColorService>;
     let drawingServiceMock: jasmine.SpyObj<DrawingService>;
+    let transformSpy: jasmine.SpyObj<any>;
 
     beforeEach(() => {
         colorServiceMock = jasmine.createSpyObj('ColorService', ['numeralToHex', 'getColor']);
         drawingServiceMock = jasmine.createSpyObj('DrawingService', ['baseCtx', 'previewCtx']);
-        // baseStubCtx = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+        baseStubCtx = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
         // previewStubCtx = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
         TestBed.configureTestingModule({
             providers: [
@@ -28,6 +29,8 @@ describe('SprayService', () => {
         sprayService = TestBed.inject(SprayService);
         colorServiceMock = TestBed.inject(ColorService) as jasmine.SpyObj<ColorService>;
         drawingServiceMock = TestBed.inject(DrawingService) as jasmine.SpyObj<DrawingService>;
+        drawingServiceMock.baseCtx = baseStubCtx;
+        transformSpy = spyOn<any>(sprayService, 'transform').and.stub();
     });
 
     it('should be created', () => {
@@ -37,5 +40,17 @@ describe('SprayService', () => {
     it('should generate random number', () => {
         const numberGenerated = sprayService.generateRandomValue(0, 3);
         expect(numberGenerated).toBeLessThanOrEqual(3);
+    });
+
+    it('should setTimeout and set mouseDown', () => {
+        const event = { x: 15, y: 6 } as MouseEvent;
+        sprayService.onMouseUp(event);
+        expect(sprayService.mouseDown).toEqual(false);
+    });
+
+    it('should not call transform if mouseDown false ', () => {
+        const event = { x: 15, y: 6 } as MouseEvent;
+        sprayService.onMouseDown(event);
+        expect(transformSpy).not.toHaveBeenCalled();
     });
 });
