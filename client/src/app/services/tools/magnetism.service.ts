f3542502ffe6126e3670dffa458d45dc478da1a5
Tai Chen Li <dev@taichen.ca>
2020-11-27 14:11:47 -0500

    added more ui elements

--- client/src/app/services/tools/magnetism.service.ts
+++ client/src/app/services/tools/magnetism.service.ts
@@ -9,10 +9,12 @@ export class MagnetismService {
     private horizontalPosition: MatSnackBarHorizontalPosition = 'right';
     private verticalPosition: MatSnackBarVerticalPosition = 'bottom';
 
+    private imageToMagnet: HTMLImageElement;
+
     constructor(private gridService: GridService, private snackBar: MatSnackBar) {}
 
     activateMagnetism(): void {
-        // const gridInfo: ImageData = this.gridService.getGridData();
+        const gridInfo: ImageData = this.gridService.getGridData();
         this.snackBar.open("Vous avez besoin d'activer la grille avec le bouton «grille» et peser sur «m» pour activer le magnétisme.", 'ok', {
             duration: 10000,
             horizontalPosition: this.horizontalPosition,
