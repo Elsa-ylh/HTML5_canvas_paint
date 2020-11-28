df3ff2e64a1bce7a1df66c1f2c563583719feff6
Tai Chen Li <dev@taichen.ca>
2020-11-28 17:36:49 -0500

    refactoring from variables to a map for ControlGroup

--- client/src/app/classes/control-group.ts
+++ client/src/app/classes/control-group.ts
@@ -4,139 +4,95 @@ import { Vec2 } from './vec2';
 
 export class ControlGroup {
     drawingService: DrawingService;
-    controlPointTop: ControlPoint;
-    controlPointBottom: ControlPoint;
-    controlPointLeft: ControlPoint;
-    controlPointRight: ControlPoint;
-    controlPointTopLeft: ControlPoint;
-    controlPointTopRight: ControlPoint;
-    controlPointBottomRight: ControlPoint;
-    controlPointBottomLeft: ControlPoint;
+    controlPoints: Map<ControlPointName, ControlPoint> = new Map<ControlPointName, ControlPoint>();
 
     constructor(drawingService: DrawingService) {
         this.drawingService = drawingService;
 
-        this.controlPointTopLeft = new ControlPoint(this.drawingService);
-        this.controlPointTop = new ControlPoint(this.drawingService);
-        this.controlPointTopRight = new ControlPoint(this.drawingService);
+        this.controlPoints.set(ControlPointName.topLeft, new ControlPoint(this.drawingService));
+        this.controlPoints.set(ControlPointName.top, new ControlPoint(this.drawingService));
+        this.controlPoints.set(ControlPointName.topRight, new ControlPoint(this.drawingService));
 
-        this.controlPointLeft = new ControlPoint(this.drawingService);
-        this.controlPointRight = new ControlPoint(this.drawingService);
+        this.controlPoints.set(ControlPointName.left, new ControlPoint(this.drawingService));
+        this.controlPoints.set(ControlPointName.right, new ControlPoint(this.drawingService));
 
-        this.controlPointBottomRight = new ControlPoint(this.drawingService);
-        this.controlPointBottom = new ControlPoint(this.drawingService);
-        this.controlPointBottomLeft = new ControlPoint(this.drawingService);
+        this.controlPoints.set(ControlPointName.bottomRight, new ControlPoint(this.drawingService));
+        this.controlPoints.set(ControlPointName.bottom, new ControlPoint(this.drawingService));
+        this.controlPoints.set(ControlPointName.bottomLeft, new ControlPoint(this.drawingService));
     }
 
     draw(): void {
-        this.controlPointTopLeft.draw();
-        this.controlPointTop.draw();
-        this.controlPointTopRight.draw();
-
-        this.controlPointLeft.draw();
-        this.controlPointRight.draw();
-
-        this.controlPointBottomRight.draw();
-        this.controlPointBottom.draw();
-        this.controlPointBottomLeft.draw();
+        for (let entry of Array.from(this.controlPoints.entries())) {
+            const controlPoint = entry[1];
+            controlPoint.draw();
+        }
     }
 
     setPositions(startingPos: Vec2, endingPos: Vec2, size: Vec2): void {
-        this.controlPointTopLeft.setPosition({ x: startingPos.x - CPSIZE / 2, y: startingPos.y - CPSIZE / 2 });
-        this.controlPointTop.setPosition({ x: startingPos.x + size.x / 2 - CPSIZE / 2, y: startingPos.y - CPSIZE / 2 });
-        this.controlPointTopRight.setPosition({ x: startingPos.x + size.x - CPSIZE / 2, y: startingPos.y - CPSIZE / 2 });
+        (this.controlPoints.get(ControlPointName.topLeft) as ControlPoint).setPosition({
+            x: startingPos.x - CPSIZE / 2,
+            y: startingPos.y - CPSIZE / 2,
+        });
+        (this.controlPoints.get(ControlPointName.top) as ControlPoint).setPosition({
+            x: startingPos.x + size.x / 2 - CPSIZE / 2,
+            y: startingPos.y - CPSIZE / 2,
+        });
+        (this.controlPoints.get(ControlPointName.topRight) as ControlPoint).setPosition({
+            x: startingPos.x + size.x - CPSIZE / 2,
+            y: startingPos.y - CPSIZE / 2,
+        });
 
-        this.controlPointLeft.setPosition({ x: startingPos.x - CPSIZE / 2, y: startingPos.y + size.y / 2 - CPSIZE / 2 });
-        this.controlPointRight.setPosition({
+        (this.controlPoints.get(ControlPointName.left) as ControlPoint).setPosition({
+            x: startingPos.x - CPSIZE / 2,
+            y: startingPos.y + size.y / 2 - CPSIZE / 2,
+        });
+        (this.controlPoints.get(ControlPointName.right) as ControlPoint).setPosition({
             x: startingPos.x + size.x - CPSIZE / 2,
             y: startingPos.y + size.y / 2 - CPSIZE / 2,
         });
 
-        this.controlPointBottomRight.setPosition({ x: endingPos.x - CPSIZE / 2, y: endingPos.y - CPSIZE / 2 });
-        this.controlPointBottom.setPosition({ x: endingPos.x - size.x / 2 - CPSIZE / 2, y: endingPos.y - CPSIZE / 2 });
-        this.controlPointBottomLeft.setPosition({ x: endingPos.x - size.x - CPSIZE / 2, y: endingPos.y - CPSIZE / 2 });
+        (this.controlPoints.get(ControlPointName.bottomRight) as ControlPoint).setPosition({
+            x: endingPos.x - CPSIZE / 2,
+            y: endingPos.y - CPSIZE / 2,
+        });
+        (this.controlPoints.get(ControlPointName.bottom) as ControlPoint).setPosition({
+            x: endingPos.x - size.x / 2 - CPSIZE / 2,
+            y: endingPos.y - CPSIZE / 2,
+        });
+        (this.controlPoints.get(ControlPointName.bottomLeft) as ControlPoint).setPosition({
+            x: endingPos.x - size.x - CPSIZE / 2,
+            y: endingPos.y - CPSIZE / 2,
+        });
     }
 
-    // only one can be selected at a tim
-    // tslint:disable:cyclomatic-complexity
-    isInControlPoint(mouse: Vec2): ControlPointName {
-        if (this.controlPointTopLeft.isInside(mouse)) {
-            const temp = this.controlPointTopLeft.selected;
+    private drawControlPoint(controlPointName: ControlPointName, controlPoint: ControlPoint, mouse: Vec2): ControlPointName {
+        if (controlPoint.isInside(mouse)) {
             this.resetSelected();
-            if (temp) this.controlPointTopLeft.selected = false;
-            else this.controlPointTopLeft.selected = true;
-            this.controlPointTopLeft.draw();
-            return ControlPointName.topLeft;
-        }
-        if (this.controlPointTop.isInside(mouse)) {
-            const temp = this.controlPointTop.selected;
-            this.resetSelected();
-            if (temp) this.controlPointTop.selected = false;
-            else this.controlPointTop.selected = true;
-            this.controlPointTop.draw();
-            return ControlPointName.top;
-        }
-        if (this.controlPointTopRight.isInside(mouse)) {
-            const temp = this.controlPointTopRight.selected;
-            this.resetSelected();
-            if (temp) this.controlPointTopRight.selected = false;
-            else this.controlPointTopRight.selected = true;
-            this.controlPointTopRight.draw();
-            return ControlPointName.topRight;
-        }
-        if (this.controlPointBottomRight.isInside(mouse)) {
-            const temp = this.controlPointBottomRight.selected;
-            this.resetSelected();
-            if (temp) this.controlPointBottomRight.selected = false;
-            else this.controlPointBottomRight.selected = true;
-            this.controlPointBottomRight.draw();
-            return ControlPointName.bottomRight;
-        }
-        if (this.controlPointBottom.isInside(mouse)) {
-            const temp = this.controlPointBottom.selected;
-            this.resetSelected();
-            if (temp) this.controlPointBottom.selected = false;
-            else this.controlPointBottom.selected = true;
-            this.controlPointBottom.draw();
-            return ControlPointName.bottom;
-        }
-        if (this.controlPointBottomLeft.isInside(mouse)) {
-            const temp = this.controlPointBottomLeft.selected;
-            this.resetSelected();
-            if (temp) this.controlPointBottomLeft.selected = false;
-            else this.controlPointBottomLeft.selected = true;
-            this.controlPointBottomLeft.draw();
-            return ControlPointName.bottomLeft;
-        }
-        if (this.controlPointLeft.isInside(mouse)) {
-            const temp = this.controlPointLeft.selected;
-            this.resetSelected();
-            if (temp) this.controlPointLeft.selected = false;
-            else this.controlPointLeft.selected = true;
-            this.controlPointLeft.draw();
-            return ControlPointName.left;
-        }
-        if (this.controlPointRight.isInside(mouse)) {
-            const temp = this.controlPointRight.selected;
-            this.resetSelected();
-            if (temp) this.controlPointRight.selected = false;
-            else this.controlPointRight.selected = true;
-            this.controlPointRight.draw();
-            return ControlPointName.right;
+            if (!controlPoint.selected) controlPoint.selected = true;
+            controlPoint.draw();
+            return controlPointName;
         }
         return ControlPointName.none;
     }
 
+    // tslint:disable:cyclomatic-complexity
+    isInControlPoint(mouse: Vec2): ControlPointName {
+        for (let entry of Array.from(this.controlPoints.entries())) {
+            const controlPointName = entry[0];
+            const controlPoint = entry[1];
+            if (this.drawControlPoint(controlPointName, controlPoint, mouse) !== ControlPointName.none) {
+                return controlPointName;
+            }
+        }
+
+        this.resetSelected();
+        return ControlPointName.none;
+    }
+
     private resetSelected(): void {
-        this.controlPointTopLeft.selected = false;
-        this.controlPointTopRight.selected = false;
-        this.controlPointTop.selected = false;
-
-        this.controlPointLeft.selected = false;
-        this.controlPointRight.selected = false;
-
-        this.controlPointBottomLeft.selected = false;
-        this.controlPointBottomRight.selected = false;
-        this.controlPointBottom.selected = false;
+        for (let entry of Array.from(this.controlPoints.entries())) {
+            const controlPoint = entry[1];
+            controlPoint.selected = false;
+        }
     }
 }
