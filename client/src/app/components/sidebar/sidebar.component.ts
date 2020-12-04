fd330ed0c5704040674081b4ec687fce4149097a
TheRealFlash <john.maliha@polymtl.ca>
2020-12-04 14:13:52 -0500

    bug fix: releasing alt and wheel, still keeps angle at 1 degree

--- client/src/app/components/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar.component.ts
@@ -630,4 +630,11 @@ export class SidebarComponent {
             this.featherService.altPressed = true;
         }
     }
+
+    @HostListener('window:keyup.alt', ['$event'])
+    allReleased(event: KeyboardEvent): void {
+        if (this.toolService.currentToolName === ToolUsed.Feather) {
+            this.featherService.altPressed = false;
+        }
+    }
 }
