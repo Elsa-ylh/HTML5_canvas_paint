5fe695e7ab37bd5db8feb2f8bf9576723fea3b85
Tai Chen Li <dev@taichen.ca>
2020-12-03 23:43:18 -0500

    started testing of magnetism

--- client/src/app/services/tools/magnetism.service.spec.ts
+++ client/src/app/services/tools/magnetism.service.spec.ts
@@ -1,16 +1,38 @@
 /* tslint:disable:no-unused-variable */
 
-import { inject, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
+import { canvasTestHelper } from '@app/classes/canvas-test-helper';
+import { CanvasResizerService } from '../canvas/canvas-resizer.service';
+import { DrawingService } from '../drawing/drawing.service';
+import { GridService } from './grid.service';
 import { MagnetismService } from './magnetism.service';
 
-describe('Service: Magnetism', () => {
+let gridService: GridService;
+
+let canvasResizerService: CanvasResizerService;
+let drawingService: DrawingService;
+let magnetismService: MagnetismService;
+
+let baseCtxStub: CanvasRenderingContext2D;
+let previewCtxStub: CanvasRenderingContext2D;
+
+fdescribe('Service: Magnetism', () => {
     beforeEach(() => {
+        drawingService = new DrawingService();
+        gridService = new GridService(drawingService, canvasResizerService);
+        magnetismService = new MagnetismService(gridService);
+
         TestBed.configureTestingModule({
-            providers: [MagnetismService],
+            providers: [{ provide: GridService, useValue: gridService }],
         });
+
+        baseCtxStub = canvasTestHelper.canvas.getContext('2d') as CanvasRenderingContext2D;
+        previewCtxStub = canvasTestHelper.drawCanvas.getContext('2d') as CanvasRenderingContext2D;
+        drawingService.baseCtx = baseCtxStub;
+        drawingService.previewCtx = previewCtxStub;
     });
 
-    it('should ...', inject([MagnetismService], (service: MagnetismService) => {
-        expect(service).toBeTruthy();
-    }));
+    it('should magnetismService exist', () => {
+        expect(magnetismService).toBeTruthy();
+    });
 });
