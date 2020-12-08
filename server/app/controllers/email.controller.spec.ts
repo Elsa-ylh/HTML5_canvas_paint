55c24b2996c682d9f56a86e4bceac368455b8101
Tai Chen Li <dev@taichen.ca>
2020-12-08 01:38:43 -0500

    email controller tests finished

--- server/app/controllers/email.controller.spec.ts
+++ server/app/controllers/email.controller.spec.ts
@@ -1,65 +1,130 @@
 import { Application } from '@app/app';
-import { EmailService } from '@app/services/email.service';
 import { TYPES } from '@app/types';
 import { expect } from 'chai';
+import { ImageFormat } from '@common/communication/image-format';
 import * as supertest from 'supertest';
-import { Stubbed, testingContainer } from '../../test/test-utils';
-//import * as FormData from 'form-data';
-
-export const EVERYTHING_IS_FINE = 200;
-export const BAD_EMAIL = 412;
-export const IMAGE_EXTENSION_NOT_SAME_AS_BINARY = 412;
-export const ALL_OTHER_ERRORS = 500;
+import { testingContainer } from '../../test/test-utils';
+import { SinonSandbox } from 'sinon';
+import { Container } from 'inversify/dts/container/container';
+import * as EmailController from './email.controller';
 
 describe('Email controller', () => {
-    let emailService: Stubbed<EmailService>;
     let app: Express.Application;
+    let testsContainer: Container;
+    let testsSandbox: SinonSandbox;
+
     beforeEach(async () => {
         const [container, sandbox] = await testingContainer();
-        container.rebind(TYPES.EmailService).toConstantValue({
-            isEmailValid: sandbox.stub().returns(true),
-            isContentValid: sandbox.stub().resolves(true),
-            sendEmail: sandbox.stub().resolves(),
-        });
-        emailService = container.get(TYPES.EmailService);
-        app = container.get<Application>(TYPES.Application).app;
+        testsContainer = container;
+        testsSandbox = sandbox;
     });
 
-    it('should undefined email', async () => {
-        emailService.isEmailValid.returns(false);
-        emailService.isContentValid.resolves(false);
+    it('should someone send a request without any formData, resolve as 400', async () => {
+        testsContainer.rebind(TYPES.EmailService).toConstantValue({
+            isEmailValid: testsSandbox.stub().returns(false),
+            isContentValid: testsSandbox.stub().resolves(false),
+            getImageExtension: testsSandbox.stub().resolves(ImageFormat.PNG),
+            sendEmail: testsSandbox.stub().resolves(),
+        });
+        app = testsContainer.get<Application>(TYPES.Application).app;
+
         return supertest(app)
             .post('/api/email')
-            .expect(400)
+            .expect(EmailController.FORMDATA_IS_MISSING)
             .then((response: any) => {
                 expect(response.text).to.deep.equal("Votre requête a besoin d'un courriel.");
             });
     });
 
-    it('should image be sent is good', async () => {
-        emailService.isEmailValid.returns(false);
-        emailService.isContentValid.resolves(false);
-        // const formData = new FormData();
-        // formData.append('payload', blob, 'aaa/image/jbg');
-        // formData.append('to', 'a@a.com');
+    it('should someone send a request without an image in formData, resolve as 400', async () => {
+        testsContainer.rebind(TYPES.EmailService).toConstantValue({
+            isEmailValid: testsSandbox.stub().returns(true),
+            isContentValid: testsSandbox.stub().resolves(true),
+            getImageExtension: testsSandbox.stub().resolves(ImageFormat.PNG),
+            sendEmail: testsSandbox.stub().resolves(),
+        });
 
         return supertest(app)
-            .post('/api/email').attach('payload', './default.jpeg').field('to', 'a@a.com')
-            .expect(400)
+            .post('/api/email')
+            .field('email', 'lol@lol.com')
+            .expect(EmailController.BAD_IMAGE)
             .then((response: any) => {
                 expect(response.text).to.deep.equal("Votre requête a besoin d'une image PNG ou JPG.");
             });
     });
 
-    it('should undefined imageFile because no formData was sent', async () => {
-        emailService.isEmailValid.returns(false);
-        emailService.isContentValid.resolves(false);
+    it('should image is good but email is invalid as not of email format, resolve as 400', async () => {
+        testsContainer.rebind(TYPES.EmailService).toConstantValue({
+            isEmailValid: testsSandbox.stub().returns(false),
+            isContentValid: testsSandbox.stub().resolves(true),
+            getImageExtension: testsSandbox.stub().resolves(ImageFormat.PNG),
+            sendEmail: testsSandbox.stub().resolves(),
+        });
 
         return supertest(app)
             .post('/api/email')
-            .expect(500)
+            .field('email', 'lol@-dwadf...lol.com')
+            .attach('image', './default.png')
+            .expect(EmailController.BAD_EMAIL)
             .then((response: any) => {
-                expect(response.text).to.deep.equal("Votre requête a besoin d'un courriel.");
+                expect(response.text).to.deep.equal("Le courriel fourni n'est pas d'un format valide. Le courriel doit être style abc@email.com");
+            });
+    });
+
+    it('should email is good but image is a fake png, resolve as 400', async () => {
+        testsContainer.rebind(TYPES.EmailService).toConstantValue({
+            isEmailValid: testsSandbox.stub().returns(true),
+            isContentValid: testsSandbox.stub().resolves(false),
+            getImageExtension: testsSandbox.stub().resolves(ImageFormat.PNG),
+            sendEmail: testsSandbox.stub().resolves(),
+        });
+        app = testsContainer.get<Application>(TYPES.Application).app;
+
+        return supertest(app)
+            .post('/api/email')
+            .field('email', 'lol@lol.com')
+            .attach('image', './default.jpeg')
+            .expect(EmailController.BAD_IMAGE)
+            .then((response: any) => {
+                expect(response.text).to.deep.equal("L'extension du fichier n'est pas le même que le contenu.");
+            });
+    });
+
+    it('should image of png format and email sent are good, resolve as 200', async () => {
+        testsContainer.rebind(TYPES.EmailService).toConstantValue({
+            isEmailValid: testsSandbox.stub().returns(true),
+            isContentValid: testsSandbox.stub().resolves(true),
+            getImageExtension: testsSandbox.stub().resolves(ImageFormat.PNG),
+            sendEmail: testsSandbox.stub().resolves(),
+        });
+        app = testsContainer.get<Application>(TYPES.Application).app;
+
+        return supertest(app)
+            .post('/api/email')
+            .field('email', 'lol@lol.com')
+            .attach('image', './default.png')
+            .expect(EmailController.EVERYTHING_IS_FINE)
+            .then((response: any) => {
+                expect(response.text).to.deep.equal("Si le courriel existe, l'image devrait se rendre au courriel dans un instant.");
+            });
+    });
+
+    it('should image of jpg format and email sent are good, resolve as 200', async () => {
+        testsContainer.rebind(TYPES.EmailService).toConstantValue({
+            isEmailValid: testsSandbox.stub().returns(true),
+            isContentValid: testsSandbox.stub().resolves(true),
+            getImageExtension: testsSandbox.stub().resolves(ImageFormat.PNG),
+            sendEmail: testsSandbox.stub().resolves(),
+        });
+        app = testsContainer.get<Application>(TYPES.Application).app;
+
+        return supertest(app)
+            .post('/api/email')
+            .field('email', 'lol@lol.com')
+            .attach('image', './default.jpeg')
+            .expect(EmailController.EVERYTHING_IS_FINE)
+            .then((response: any) => {
+                expect(response.text).to.deep.equal("Si le courriel existe, l'image devrait se rendre au courriel dans un instant.");
             });
     });
 });
--- server/app/controllers/email.controller.ts
+++ server/app/controllers/email.controller.ts
@@ -7,8 +7,9 @@ import { inject, injectable } from 'inversify';
 import { TYPES } from '../types';
 
 export const EVERYTHING_IS_FINE = 200;
-export const BAD_EMAIL = 412;
-export const IMAGE_EXTENSION_NOT_SAME_AS_BINARY = 412;
+export const FORMDATA_IS_MISSING = 400;
+export const BAD_IMAGE = 400;
+export const BAD_EMAIL = 400;
 export const ALL_OTHER_ERRORS = 500;
 
 @injectable()
@@ -24,23 +25,20 @@ export class EmailController {
             const email = req.body.email;
 
             if (email === undefined) {
-                res.status(400).send("Votre requête a besoin d'un courriel.");
+                console.log("Votre requête a besoin d'un courriel.");
+                res.status(BAD_EMAIL).send("Votre requête a besoin d'un courriel.");
                 return;
             }
 
             // Multer request type
             // tslint:disable-next-line:no-any
             let imageFile;
-            try {
-                imageFile = (req as any).file;
-                console.log(imageFile);
-            } catch (error) {
-                console.log(error);
-                res.status(400).send("Votre requête a besoin d'une image File.");
-            }
+            imageFile = (req as any).file;
+            console.log(imageFile);
 
             if (imageFile === undefined) {
-                res.status(400).send("Votre requête a besoin d'une image PNG ou JPG.");
+                console.log("Votre requête a besoin d'une image PNG ou JPG.");
+                res.status(BAD_IMAGE).send("Votre requête a besoin d'une image PNG ou JPG.");
             }
 
             const expressImageName = imageFile.path as string;
@@ -57,25 +55,19 @@ export class EmailController {
             const isImageContentEqualExtension: boolean = await this.emailService.isContentValid(expressImageName, expectedFileExtension);
             if (!isImageContentEqualExtension) {
                 console.log("L'extension du fichier n'est pas le même que le contenu.");
-                res.status(IMAGE_EXTENSION_NOT_SAME_AS_BINARY).send("L'extension du fichier n'est pas le même que le contenu.");
+                res.status(BAD_IMAGE).send("L'extension du fichier n'est pas le même que le contenu.");
                 return;
             }
 
-            fs.rename(expressImageName, properImageName, (err) => {
-                if (err) {
-                    console.log(err);
-                    res.status(ALL_OTHER_ERRORS).send('Edge cases reached, do not attempt it again.');
-                    return;
-                } else {
-                    const formData = new FormData();
-                    formData.append('to', email);
-                    formData.append('payload', fs.createReadStream(properImageName));
+            fs.rename(expressImageName, properImageName, () => {
+                const formData = new FormData();
+                formData.append('to', email);
+                formData.append('payload', fs.createReadStream(properImageName));
 
-                    this.emailService.sendEmail(formData);
+                this.emailService.sendEmail(formData);
 
-                    res.status(EVERYTHING_IS_FINE).send("Si le courriel existe, l'image devrait se rendre au courriel dans un instant.");
-                    return;
-                }
+                res.status(EVERYTHING_IS_FINE).send("Si le courriel existe, l'image devrait se rendre au courriel dans un instant.");
+                return;
             });
         });
     }
