3b72b08f542bde11a1b05c21e739e4382b9afe09
Tai Chen Li <dev@taichen.ca>
2020-12-08 20:39:55 -0500

    color error linting fix

--- client/src/app/components/color-error/color-error.component.spec.ts
+++ client/src/app/components/color-error/color-error.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog/dialog-module';
 import { ColorErrorComponent } from '@app/components/color-error/color-error.component';
 
 let component: ColorErrorComponent;
@@ -7,6 +9,7 @@ let fixture: ComponentFixture<ColorErrorComponent>;
 describe('ColorErrorComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
+            imports: [MatButtonModule, MatDialogModule],
             declarations: [ColorErrorComponent],
         }).compileComponents();
     });
--- client/src/app/components/color-error/color-error.component.ts
+++ client/src/app/components/color-error/color-error.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-
-// tslint:disable:no-empty
-
 @Component({
     selector: 'app-color-error',
     templateUrl: './color-error.component.html',
     styleUrls: ['./color-error.component.scss'],
 })
-export class ColorErrorComponent {
-}
+export class ColorErrorComponent {}
