814d91fc996403eaf3e14c0d8d8edcc287738185
alphonse <alphonse.desrosiers@polymtl.ca>
2020-12-09 14:13:38 -0500

    start test read file

---
+++ server/app/services/read-file.service.spec.ts
@@ -0,0 +1,9 @@
+import { ReadFileService } from './read-file.service';
+//import { expect } from 'chai';
+describe('Email service', () => {
+    let readFileService: ReadFileService;
+
+    beforeEach(async () => {
+        readFileService = new ReadFileService();
+    });
+});
